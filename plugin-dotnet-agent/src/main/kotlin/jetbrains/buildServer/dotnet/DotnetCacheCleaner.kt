5182f4b03b52b32c22ea01c564d7177eb24f3799
Dmitry.Tretyakov <dtretyakov@gmail.com>
2016-07-22 18:00:11 +0300

    Provide nuget packages cleaner
    Issue: #11

---
+++ plugin-dotnet-agent/src/main/kotlin/jetbrains/buildServer/dotnet/DotnetCacheCleaner.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2000-2016 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * See LICENSE in the project root for license information.
+ */
+
+package jetbrains.buildServer.dotnet
+
+import com.intellij.openapi.diagnostic.Logger
+import jetbrains.buildServer.agent.DirectoryCleanersProvider
+import jetbrains.buildServer.agent.DirectoryCleanersProviderContext
+import jetbrains.buildServer.agent.DirectoryCleanersRegistry
+
+import java.io.File
+import java.util.Date
+
+/**
+ * Cleans up nuget package and cache directories.
+ */
+class DotnetCacheCleaner : DirectoryCleanersProvider {
+
+    override fun getCleanerName() = "nuget cache cleaner"
+
+    override fun registerDirectoryCleaners(context: DirectoryCleanersProviderContext,
+                                           registry: DirectoryCleanersRegistry) {
+        System.getenv("NUGET_PACKAGES")?.let { path ->
+            val globalPackages = File(path)
+            if (globalPackages.isAbsolute) {
+                LOG.info("Registering directory $globalPackages for cleaning")
+                registry.addCleaner(globalPackages, Date())
+            }
+        }
+
+        System.getenv("LOCALAPPDATA")?.let { path ->
+            val nugetCache = File(path, "NuGet/Cache")
+            LOG.info("Registering directory $nugetCache for cleaning")
+            registry.addCleaner(nugetCache, Date())
+
+            val nugetv3Cache = File(path, "NuGet/v3-cache")
+            LOG.info("Registering directory $nugetv3Cache for cleaning")
+            registry.addCleaner(nugetv3Cache, Date())
+        }
+
+        System.getProperty("user.home")?.let { path ->
+            val nugetPackages = File(path, ".nuget/packages")
+            LOG.info("Registering directory $nugetPackages for cleaning")
+            registry.addCleaner(nugetPackages, Date())
+        }
+    }
+
+    companion object {
+        private val LOG = Logger.getInstance(DotnetCacheCleaner::class.java.name)
+    }
+}
