8308bfe963269383824a651a88ddf60156780c0e
Dmitry.Tretyakov <dtretyakov@gmail.com>
2017-02-15 12:20:10 +0300

    Update copyright to 2017

--- plugin-dotnet-server/src/main/kotlin/jetbrains/buildServer/dotnet/commands/dotnet/BuildCommandType.kt
+++ plugin-dotnet-server/src/main/kotlin/jetbrains/buildServer/dotnet/commands/dotnet/BuildCommandType.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * See LICENSE in the project root for license information.
