38b9393db7b391eee7b0398a4d5dc38575041c07
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2017-09-11 19:23:19 +0300

    Change name of vstest/msbuild versions

--- plugin-dotnet-common/src/main/kotlin/jetbrains/buildServer/dotnet/Tool.kt
+++ plugin-dotnet-common/src/main/kotlin/jetbrains/buildServer/dotnet/Tool.kt
@@ -2,22 +2,22 @@ package jetbrains.buildServer.dotnet
 
 enum class Tool(val version: Int, val type: ToolType, val platform: ToolPlatform, val bitness: ToolBitness, val description: String) {
     // MSBuild
-    MSBuild15CrossPlatform(15, ToolType.MSBuild, ToolPlatform.Any, ToolBitness.Any, "Cross-platform MSBuild"),
-    MSBuild15Windows(15, ToolType.MSBuild, ToolPlatform.Windows, ToolBitness.Any, "MSBuild v15"),
-    MSBuild15WindowsX64(15, ToolType.MSBuild, ToolPlatform.Windows, ToolBitness.x64, "MSBuild v15 64-bit"),
-    MSBuild15WindowsX86(15, ToolType.MSBuild, ToolPlatform.Windows, ToolBitness.x86, "MSBuild v15 32-bit"),
-    MSBuild14Windows(14, ToolType.MSBuild, ToolPlatform.Windows, ToolBitness.Any, "MSBuild v14"),
-    MSBuild14WindowsX64(14, ToolType.MSBuild, ToolPlatform.Windows, ToolBitness.x64, "MSBuild v14 64-bit"),
-    MSBuild14WindowsX86(14, ToolType.MSBuild, ToolPlatform.Windows, ToolBitness.x86, "MSBuild v14 32-bit"),
-    MSBuild12Windows(12, ToolType.MSBuild, ToolPlatform.Windows, ToolBitness.Any, "MSBuild v12"),
-    MSBuild12WindowsX64(12, ToolType.MSBuild, ToolPlatform.Windows, ToolBitness.x64, "MSBuild v12 64-bit"),
-    MSBuild12WindowsX86(12, ToolType.MSBuild, ToolPlatform.Windows, ToolBitness.x86, "MSBuild v12 32-bit"),
+    MSBuild15CrossPlatform(15, ToolType.MSBuild, ToolPlatform.Any, ToolBitness.Any, "Cross-platform"),
+    MSBuild15Windows(15, ToolType.MSBuild, ToolPlatform.Windows, ToolBitness.Any, "15"),
+    MSBuild15WindowsX64(15, ToolType.MSBuild, ToolPlatform.Windows, ToolBitness.x64, "15 64-bit"),
+    MSBuild15WindowsX86(15, ToolType.MSBuild, ToolPlatform.Windows, ToolBitness.x86, "15 32-bit"),
+    MSBuild14Windows(14, ToolType.MSBuild, ToolPlatform.Windows, ToolBitness.Any, "14"),
+    MSBuild14WindowsX64(14, ToolType.MSBuild, ToolPlatform.Windows, ToolBitness.x64, "14 64-bit"),
+    MSBuild14WindowsX86(14, ToolType.MSBuild, ToolPlatform.Windows, ToolBitness.x86, "14 32-bit"),
+    MSBuild12Windows(12, ToolType.MSBuild, ToolPlatform.Windows, ToolBitness.Any, "12"),
+    MSBuild12WindowsX64(12, ToolType.MSBuild, ToolPlatform.Windows, ToolBitness.x64, "12 64-bit"),
+    MSBuild12WindowsX86(12, ToolType.MSBuild, ToolPlatform.Windows, ToolBitness.x86, "12 32-bit"),
 
     // VSTest
-    VSTest15CrossPlatform(15, ToolType.VSTest, ToolPlatform.Any, ToolBitness.Any, "Cross-platform VSTest"),
-    VSTest15Windows(15, ToolType.VSTest, ToolPlatform.Windows, ToolBitness.Any, "VSTest v15"),
-    VSTest14Windows(14, ToolType.VSTest, ToolPlatform.Windows, ToolBitness.Any, "VSTest v14"),
-    VSTest12Windows(12, ToolType.VSTest, ToolPlatform.Windows, ToolBitness.Any, "VSTest v12");
+    VSTest15CrossPlatform(15, ToolType.VSTest, ToolPlatform.Any, ToolBitness.Any, "Cross-platform"),
+    VSTest15Windows(15, ToolType.VSTest, ToolPlatform.Windows, ToolBitness.Any, "15"),
+    VSTest14Windows(14, ToolType.VSTest, ToolPlatform.Windows, ToolBitness.Any, "14"),
+    VSTest12Windows(12, ToolType.VSTest, ToolPlatform.Windows, ToolBitness.Any, "12");
 
     public val id: String get() = "${type}_${version}_${platform}_${bitness}"
 
