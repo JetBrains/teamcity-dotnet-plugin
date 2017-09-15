f8cfb1d5951f07dd7955ff3fc4674a6578b38fd4
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2017-09-15 15:56:07 +0300

    #69 Can't install the Dotnet Integration Tool after installing plugin - fix issue, change order of versions

--- plugin-dotnet-server/src/main/kotlin/jetbrains/buildServer/dotnet/DotnetToolProviderAdapter.kt
+++ plugin-dotnet-server/src/main/kotlin/jetbrains/buildServer/dotnet/DotnetToolProviderAdapter.kt
@@ -15,22 +15,14 @@ class DotnetToolProviderAdapter(
         private val _nuGetService: NuGetService,
         private val _fileSystemService: FileSystemService): ServerToolProviderAdapter() {
 
-    override fun getType(): jetbrains.buildServer.tools.ToolType {
-        return DotnetToolTypeAdapter.Shared;
-    }
+    override fun getType(): jetbrains.buildServer.tools.ToolType = DotnetToolTypeAdapter.Shared;
 
-    override fun getAvailableToolVersions(): MutableCollection<out ToolVersion> {
-        return tools.toMutableList()
-    }
+    override fun getAvailableToolVersions(): MutableCollection<out ToolVersion> = tools.toMutableList()
 
-    override fun tryGetPackageVersion(toolPackage: File): GetPackageVersionResult {
-        val packageVersion = _packageVersionParser.tryParse(toolPackage.name)
-        if (packageVersion == null) {
-            return GetPackageVersionResult.error("Failed to get version of " + toolPackage)
-        }
-
-        return GetPackageVersionResult.version(DotnetToolVersion(packageVersion.toString()))
-    }
+    override fun tryGetPackageVersion(toolPackage: File): GetPackageVersionResult =
+        _packageVersionParser.tryParse(toolPackage.name)?.let {
+            GetPackageVersionResult.version(DotnetToolVersion(it.toString()))
+        } ?: GetPackageVersionResult.error("Failed to get version of " + toolPackage)
 
     override fun fetchToolPackage(toolVersion: ToolVersion, targetDirectory: File): File {
         val downloadableTool = tools
@@ -45,14 +37,15 @@ class DotnetToolProviderAdapter(
         try {
             _fileSystemService
                     .createOutputFile(targetFile)
-                    .use({
+                    .use {
                         fileStream -> _httpDownloader.download(URL(downloadUrl), fileStream)
-                        fileStream.flush() })
+                        fileStream.flush()
+                    }
+
+            return targetFile
         } catch (e: Throwable) {
             throw ToolException("Failed to download package " + toolVersion + " to " + targetFile + e.message, e)
         }
-
-        return targetFile
     }
 
     override fun unpackToolPackage(toolPackage: File, targetDirectory: File) {
@@ -67,27 +60,21 @@ class DotnetToolProviderAdapter(
     }
 
     private val tools: List<DotnetTool> get() {
-        var listOfPackages: List<DotnetTool>
         try {
-            listOfPackages = _nuGetService.getPackagesById(type.type, true).filter { it.isListed == true }.map { DotnetTool(it) }.toList()
+            return _nuGetService.getPackagesById(type.type, true).filter { it.isListed == true }.map { DotnetTool(it) }.toList().reversed()
         } catch (e: Throwable) {
             throw ToolException("Failed to download list of packages for ${type.type}: " + e.message, e)
         }
-
-        return listOfPackages
     }
 
     private inner class DotnetToolVersion internal constructor(version: String)
-        : SimpleToolVersion(
-            type,
-            version,
-            ToolVersionIdHelper.getToolId(DotnetConstants.PACKAGE_TYPE, version))
+        : SimpleToolVersion(type, version, ToolVersionIdHelper.getToolId(DotnetConstants.PACKAGE_TYPE, version))
 
     companion object {
         private val LOG: Logger = Logger.getInstance(DotnetToolProviderAdapter::class.java.name)
         private val NUGET_PACKAGE_FILE_FILTER = FileFilter { pathname ->
             val name = pathname.name
-            pathname.isFile && name.startsWith(DotnetConstants.PACKAGE_TYPE) && name.endsWith(DotnetConstants.PACKAGE_NUGET_EXTENSION)
+            pathname.isFile && name.startsWith(DotnetConstants.PACKAGE_TYPE, true) && name.endsWith(DotnetConstants.PACKAGE_NUGET_EXTENSION, true)
         }
     }
 }
