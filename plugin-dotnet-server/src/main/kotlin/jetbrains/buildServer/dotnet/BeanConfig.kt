7b1c1f12f96c1e3018056076646cceb883caeafb
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2017-10-20 16:51:12 +0300

    #98 Warning in server log while start

--- plugin-dotnet-server/src/main/kotlin/jetbrains/buildServer/dotnet/BeanConfig.kt
+++ plugin-dotnet-server/src/main/kotlin/jetbrains/buildServer/dotnet/BeanConfig.kt
@@ -1,5 +1,6 @@
 package jetbrains.buildServer.dotnet
 
+import jetbrains.buildServer.tools.installed.InstalledToolsState
 import jetbrains.buildServer.web.openapi.PluginDescriptor
 import org.springframework.beans.factory.support.AbstractBeanFactory
 import org.springframework.cache.CacheManager
@@ -11,6 +12,7 @@ import java.time.Duration
 @Configuration
 @EnableCaching
 open class BeanConfig(
+        installedToolsState: InstalledToolsState,
         beanFactory: AbstractBeanFactory,
         parameters: DotnetParametersProvider,
         pluginDescriptor: PluginDescriptor,
@@ -24,6 +26,12 @@ open class BeanConfig(
             beanFactory.registerSingleton(
                     DotnetToolProviderAdapter::class.java.name,
                     DotnetToolProviderAdapter(pluginDescriptor, packageVersionParser, httpDownloader, nuGetService, _fileSystemService))
+        }
+        else {
+            val toolIds = installedToolsState.all.filter { it.toolType == DotnetConstants.PACKAGE_TYPE }.map { it.toolId }.toList()
+            for (toolId in toolIds) {
+                installedToolsState.remove(toolId)
+            }
         }
     }
 
