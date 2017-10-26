520a94986312012289f997328e5352325d25f27a
Dmitry Tretyakov <dtretyakov@gmail.com>
2017-10-27 00:04:00 +0200

    Plugin fails to start on Java 6
    Simplify regex to match version numbers
    
    Issue: TW-52109

--- plugin-dotnet-common/src/main/kotlin/jetbrains/buildServer/dotnet/SemanticVersionParserImpl.kt
+++ plugin-dotnet-common/src/main/kotlin/jetbrains/buildServer/dotnet/SemanticVersionParserImpl.kt
@@ -1,20 +1,19 @@
 package jetbrains.buildServer.dotnet
 
 class SemanticVersionParserImpl : SemanticVersionParser {
-    override fun tryParse(version: String): SemanticVersion? {
-        val matcher = VersionPattern.matcher(version)
-        if(!matcher.find() || matcher.groupCount() < 5) {
-            return null
-        }
 
-        return SemanticVersion(
-                matcher.group("major").toInt(),
-                matcher.group("minor").toInt(),
-                matcher.group("build").toInt(),
-                matcher.group("buildName") ?: "")
+    override fun tryParse(version: String): SemanticVersion? {
+        VERSION_PATTERN.find(version)?.let {
+            val (_, major, minor, build, _, patch, _) = it.destructured
+            return SemanticVersion(major.toInt(), minor.toInt(), build.toInt(), patch)
+        }
+        return null
     }
 
     companion object {
-        val VersionPattern = Regex("(^[a-z.]+\\.|^)(?<major>[\\d]+)\\.(?<minor>[\\d]+)\\.(?<build>[\\d]+)(-(?<buildName>\\w+)|)(.nupkg|)$", RegexOption.IGNORE_CASE).toPattern()
+        val VERSION_PATTERN = Regex(
+                """^([a-z.]+\.)?(\d+)\.(\d+)\.(\d+)(-([a-z]+))?(\.nupkg)?${'$'}""",
+                RegexOption.IGNORE_CASE
+        )
     }
 }
