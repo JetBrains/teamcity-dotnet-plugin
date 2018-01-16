decc5eb983a9ce7f14c7ad5682b455ac2159a80b
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2018-01-16 16:17:00 +0300

    a

--- plugin-dotnet-agent/src/main/kotlin/jetbrains/buildServer/dotnet/DotnetCommonArgumentsProviderImpl.kt
+++ plugin-dotnet-agent/src/main/kotlin/jetbrains/buildServer/dotnet/DotnetCommonArgumentsProviderImpl.kt
@@ -3,12 +3,15 @@ package jetbrains.buildServer.dotnet
 import jetbrains.buildServer.agent.CommandLineArgument
 import jetbrains.buildServer.agent.runner.ParameterType
 import jetbrains.buildServer.agent.runner.ParametersService
+import jetbrains.buildServer.dotnet.DotnetConstants.PARAM_RSP
 import kotlin.coroutines.experimental.buildSequence
 
 class DotnetCommonArgumentsProviderImpl(
         private val _parametersService: ParametersService,
         private val _responseFileArgumentsProvider: ArgumentsProvider,
-        private val _customArgumentsProvider: ArgumentsProvider)
+        private val _customArgumentsProvider: ArgumentsProvider,
+        private val _msBuildVSTestLoggerParametersProvider: MSBuildParametersProvider,
+        private val _msBuildParameterConverter: MSBuildParameterConverter)
     : DotnetCommonArgumentsProvider {
     override val arguments: Sequence<CommandLineArgument>
         get() = buildSequence {
@@ -20,7 +23,13 @@ class DotnetCommonArgumentsProviderImpl(
             }
 
             yieldAll(_customArgumentsProvider.arguments)
-            yieldAll(_responseFileArgumentsProvider.arguments)
+            val useRspFiles = _parametersService.tryGetParameter(ParameterType.Configuration, PARAM_RSP)?.let { it.equals("false", true) } ?: true;
+            if (useRspFiles) {
+                yieldAll(_responseFileArgumentsProvider.arguments)
+            }
+            else {
+                yieldAll(_msBuildVSTestLoggerParametersProvider.parameters.map { CommandLineArgument( _msBuildParameterConverter.convert(it)) })
+            }
         }
 
     private fun parameters(parameterName: String): String? = _parametersService.tryGetParameter(ParameterType.Runner, parameterName)
