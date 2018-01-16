09c77cb30fbb96a90e84c6de5ec93e365a1046b7
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2018-01-16 16:55:00 +0300

    #116 restore fails for dotnet SDK 1.x

--- plugin-dotnet-agent/src/main/kotlin/jetbrains/buildServer/dotnet/DotnetCommonArgumentsProviderImpl.kt
+++ plugin-dotnet-agent/src/main/kotlin/jetbrains/buildServer/dotnet/DotnetCommonArgumentsProviderImpl.kt
@@ -11,6 +11,7 @@ class DotnetCommonArgumentsProviderImpl(
         private val _responseFileArgumentsProvider: ArgumentsProvider,
         private val _customArgumentsProvider: ArgumentsProvider,
         private val _msBuildVSTestLoggerParametersProvider: MSBuildParametersProvider,
+        private val _msBuildLoggerArgumentsProvider: ArgumentsProvider,
         private val _msBuildParameterConverter: MSBuildParameterConverter)
     : DotnetCommonArgumentsProvider {
     override val arguments: Sequence<CommandLineArgument>
@@ -23,11 +24,12 @@ class DotnetCommonArgumentsProviderImpl(
             }
 
             yieldAll(_customArgumentsProvider.arguments)
-            val useRspFiles = _parametersService.tryGetParameter(ParameterType.Configuration, PARAM_RSP)?.let { it.equals("false", true) } ?: true;
-            if (useRspFiles) {
+            val avoidUsingRspFiles = _parametersService.tryGetParameter(ParameterType.Configuration, PARAM_RSP)?.let { it.equals("false", true) } ?: false;
+            if (!avoidUsingRspFiles) {
                 yieldAll(_responseFileArgumentsProvider.arguments)
             }
             else {
+                yieldAll(_msBuildLoggerArgumentsProvider.arguments)
                 yieldAll(_msBuildVSTestLoggerParametersProvider.parameters.map { CommandLineArgument( _msBuildParameterConverter.convert(it)) })
             }
         }
--- plugin-dotnet-agent/src/test/kotlin/jetbrains/buildServer/dotnet/test/dotnet/DotnetCommonArgumentsProviderTest.kt
+++ plugin-dotnet-agent/src/test/kotlin/jetbrains/buildServer/dotnet/test/dotnet/DotnetCommonArgumentsProviderTest.kt
@@ -1,9 +1,10 @@
 package jetbrains.buildServer.dotnet.test.dotnet
 
-import jetbrains.buildServer.dotnet.DotnetCommonArgumentsProviderImpl
-import jetbrains.buildServer.dotnet.DotnetConstants
-import jetbrains.buildServer.dotnet.Verbosity
+import jetbrains.buildServer.agent.CommandLineArgument
+import jetbrains.buildServer.dotnet.*
 import jetbrains.buildServer.dotnet.test.agent.runner.ParametersServiceStub
+import org.jmock.Expectations
+import org.jmock.Mockery
 import org.testng.Assert
 import org.testng.annotations.DataProvider
 import org.testng.annotations.Test
@@ -13,8 +14,18 @@ class DotnetCommonArgumentsProviderTest {
     fun argumentsData(): Array<Array<Any>> {
         return arrayOf(
                 arrayOf(mapOf(
+                        DotnetConstants.PARAM_VERBOSITY to Verbosity.Normal.id,
+                            DotnetConstants.PARAM_RSP to "true"),
+                        listOf("--verbosity", Verbosity.Normal.id)),
+                arrayOf(mapOf(
                         DotnetConstants.PARAM_VERBOSITY to Verbosity.Normal.id),
                         listOf("--verbosity", Verbosity.Normal.id)),
+                arrayOf(mapOf(
+                        DotnetConstants.PARAM_RSP to "false"),
+                        listOf("l:/logger", "/p:param=value")),
+                arrayOf(mapOf(
+                        DotnetConstants.PARAM_RSP to "FaLse"),
+                        listOf("l:/logger", "/p:param=value")),
                 arrayOf(emptyMap<String, String>(),
                         emptyList<String>()),
                 arrayOf(mapOf(
@@ -27,7 +38,27 @@ class DotnetCommonArgumentsProviderTest {
             parameters: Map<String, String>,
             expectedArguments: List<String>) {
         // Given
-        val argumentsProvider = DotnetCommonArgumentsProviderImpl(ParametersServiceStub(parameters), DotnetCommonArgumentsProviderStub(), DotnetCommonArgumentsProviderStub())
+        val ctx = Mockery()
+        val msBuildParametersProvider = ctx.mock(MSBuildParametersProvider::class.java)
+        val msBuildParameterConverter = ctx.mock(MSBuildParameterConverter::class.java)
+        val msBuildParameter = MSBuildParameter("Param1", "Value1")
+        ctx!!.checking(object : Expectations() {
+            init {
+                allowing<MSBuildParametersProvider>(msBuildParametersProvider).parameters
+                will(returnValue(sequenceOf(msBuildParameter)))
+
+                allowing<MSBuildParameterConverter>(msBuildParameterConverter).convert(msBuildParameter)
+                will(returnValue("/p:param=value"))
+            }
+        })
+
+        val argumentsProvider = DotnetCommonArgumentsProviderImpl(
+                ParametersServiceStub(parameters),
+                DotnetCommonArgumentsProviderStub(),
+                DotnetCommonArgumentsProviderStub(),
+                msBuildParametersProvider,
+                DotnetCommonArgumentsProviderStub(sequenceOf(CommandLineArgument("l:/logger"))),
+                msBuildParameterConverter)
 
         // When
         val actualArguments = argumentsProvider.arguments.map { it.value }.toList()
