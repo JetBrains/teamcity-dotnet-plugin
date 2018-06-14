4032a0e7207b678ace7939784ad39a21cc3dc89e
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2018-06-14 18:46:13 +0300

    #127 TW-54720 Test results not being reported with .NET CLI 2.1 release - fix the vstest scenario

--- plugin-dotnet-agent/src/test/kotlin/jetbrains/buildServer/dotnet/test/dotnet/VSTestLoggerArgumentsProviderTest.kt
+++ plugin-dotnet-agent/src/test/kotlin/jetbrains/buildServer/dotnet/test/dotnet/VSTestLoggerArgumentsProviderTest.kt
@@ -1,6 +1,11 @@
 package jetbrains.buildServer.dotnet.test.dotnet
 
+import jetbrains.buildServer.agent.runner.PathsService
+import jetbrains.buildServer.dotnet.LoggerParameters
 import jetbrains.buildServer.dotnet.VSTestLoggerArgumentsProvider
+import jetbrains.buildServer.dotnet.Verbosity
+import org.jmock.Expectations
+import org.jmock.Mockery
 import org.testng.Assert
 import org.testng.annotations.DataProvider
 import org.testng.annotations.Test
@@ -13,20 +18,39 @@ class VSTestLoggerArgumentsProviderTest {
                 // Success scenario
                 arrayOf(
                         File("loggerPath", "vstestlogger.dll") as File?,
+                        Verbosity.Normal,
                         listOf(
                                 "/logger:logger://teamcity",
-                                "/TestAdapterPath:${File("loggerPath").absolutePath}"))
+                                "/TestAdapterPath:${File("loggerPath").absolutePath}",
+                                "/logger:console;verbosity=normal")),
+
+                arrayOf(
+                        File("loggerPath", "vstestlogger.dll") as File?,
+                        Verbosity.Detailed,
+                        listOf(
+                                "/logger:logger://teamcity",
+                                "/TestAdapterPath:${File("loggerPath").absolutePath}",
+                                "/logger:console;verbosity=detailed"))
         )
     }
 
     @Test(dataProvider = "testLoggerArgumentsData")
     fun shouldGetArguments(
             loggerFile: File,
+            verbosity: Verbosity,
             expectedArguments: List<String>) {
         // Given
-        val argumentsProvider = VSTestLoggerArgumentsProvider(LoggerResolverStub(File("msbuildlogger"), loggerFile))
+        val ctx = Mockery()
+        val loggerParameters = ctx.mock(LoggerParameters::class.java)
+        val argumentsProvider = VSTestLoggerArgumentsProvider(LoggerResolverStub(File("msbuildlogger"), loggerFile), loggerParameters)
 
         // When
+        ctx.checking(object : Expectations() {
+            init {
+                oneOf<LoggerParameters>(loggerParameters).VSTestVerbosity
+                will(returnValue(verbosity))
+            }
+        })
         val actualArguments = argumentsProvider.arguments.map { it.value }.toList()
 
         // Then
