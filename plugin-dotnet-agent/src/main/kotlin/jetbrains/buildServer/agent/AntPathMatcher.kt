f8b5984dc62c95e9672e86f883ec2b174e1073f5
Nikolai Kulakov <nikolai.kulakov@jetbrains.com>
2019-03-05 15:03:19 +0300

    Fix compilation error after common library method change

--- plugin-dotnet-agent/src/main/kotlin/jetbrains/buildServer/agent/AntPathMatcher.kt
+++ plugin-dotnet-agent/src/main/kotlin/jetbrains/buildServer/agent/AntPathMatcher.kt
@@ -5,11 +5,7 @@ import java.io.File
 
 class AntPathMatcher : PathMatcher {
     override fun match(path: File, includeRules: List<String>): List<File> =
-            AntPatternFileCollector.scanDir(path, includeRules.toTypedArray(), emptyArray(), ScanOptions)
+            AntPatternFileCollector.scanDir(path, includeRules.toTypedArray(), emptyArray(),
+                    AntPatternFileCollector.ScanOption.PRIORITIZE_EXCLUDES, AntPatternFileCollector.ScanOption.ALLOW_EXTERNAL_SCAN)
 
-    companion object {
-        private val ScanOptions = arrayOf(
-                AntPatternFileCollector.ScanOption.PRIORITIZE_EXCLUDES,
-                AntPatternFileCollector.ScanOption.ALLOW_EXTERNAL_SCAN)
-    }
 }
