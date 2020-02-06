9ea6fb8d454e06e8acb07b7399becb1a574949ab
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2020-02-06 19:22:36 +0300

    Support VS 2019

--- plugin-dotnet-agent/src/main/kotlin/jetbrains/buildServer/dotnet/Logger.kt
+++ plugin-dotnet-agent/src/main/kotlin/jetbrains/buildServer/dotnet/Logger.kt
@@ -37,7 +37,11 @@ enum class Logger(val msbuildTool: Tool, val vstestTool: Tool, val msbuildLogger
     V14WindowsX64(Tool.MSBuild14WindowsX64, Tool.VSTest14Windows, File("${MSBuildLoggerSubDir}14", MSBuildLoggerAssemblyName), File("${VSTestSubDir}14", VSTestLoggerAssemblyName)),
     V14WindowsX86(Tool.MSBuild14WindowsX86, Tool.VSTest14Windows, File("${MSBuildLoggerSubDir}14", MSBuildLoggerAssemblyName), File("${VSTestSubDir}14", VSTestLoggerAssemblyName)),
 
-    V15Windows(Tool.MSBuild15Windows, Tool.VSTest15Windows, File("${MSBuildLoggerSubDir}14", MSBuildLoggerAssemblyName), File("${VSTestSubDir}14", VSTestLoggerAssemblyName)),
-    V15WindowsX64(Tool.MSBuild15WindowsX64, Tool.VSTest15Windows, File("${MSBuildLoggerSubDir}14", MSBuildLoggerAssemblyName), File("${VSTestSubDir}14", VSTestLoggerAssemblyName)),
-    V15WindowsX86(Tool.MSBuild15WindowsX86, Tool.VSTest15Windows, File("${MSBuildLoggerSubDir}14", MSBuildLoggerAssemblyName), File("${VSTestSubDir}14", VSTestLoggerAssemblyName)),
+    V15Windows(Tool.MSBuild15Windows, Tool.VSTest15Windows, File("${MSBuildLoggerSubDir}15", MSBuildLoggerAssemblyName), File("${VSTestSubDir}15", VSTestLoggerAssemblyName)),
+    V15WindowsX64(Tool.MSBuild15WindowsX64, Tool.VSTest15Windows, File("${MSBuildLoggerSubDir}15", MSBuildLoggerAssemblyName), File("${VSTestSubDir}15", VSTestLoggerAssemblyName)),
+    V15WindowsX86(Tool.MSBuild15WindowsX86, Tool.VSTest15Windows, File("${MSBuildLoggerSubDir}15", MSBuildLoggerAssemblyName), File("${VSTestSubDir}15", VSTestLoggerAssemblyName)),
+
+    V16Windows(Tool.MSBuild16Windows, Tool.VSTest16Windows, File("${MSBuildLoggerSubDir}15", MSBuildLoggerAssemblyName), File("${VSTestSubDir}15", VSTestLoggerAssemblyName)),
+    V16WindowsX64(Tool.MSBuild16WindowsX64, Tool.VSTest16Windows, File("${MSBuildLoggerSubDir}15", MSBuildLoggerAssemblyName), File("${VSTestSubDir}15", VSTestLoggerAssemblyName)),
+    V16WindowsX86(Tool.MSBuild16WindowsX86, Tool.VSTest16Windows, File("${MSBuildLoggerSubDir}15", MSBuildLoggerAssemblyName), File("${VSTestSubDir}15", VSTestLoggerAssemblyName)),
 }
