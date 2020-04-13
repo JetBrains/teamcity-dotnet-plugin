188cb1046714346774f20f5d5a60697fc5cdf01e
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2020-04-13 12:59:42 +0300

    TW-64621 Enable experimental features in .Net CLI build runner by default - rename parameter teamcity.dotnet.experimental to teamcity.internal.dotnet.experimental

--- plugin-dotnet-common/src/main/kotlin/jetbrains/buildServer/dotnet/DotnetConstants.kt
+++ plugin-dotnet-common/src/main/kotlin/jetbrains/buildServer/dotnet/DotnetConstants.kt
@@ -40,7 +40,7 @@ object DotnetConstants {
 
     // Internal configuration parameters:
     // True or False (False by default) - allows experimental features
-    const val PARAM_EXPERIMENTAL = "teamcity.dotnet.experimental"
+    const val PARAM_EXPERIMENTAL = "teamcity.internal.dotnet.experimental"
     // On, MultiAdapterPath or Off (MultiAdapterPath by default)
     const val PARAM_TEST_REPORTING = "dotnet.cli.test.reporting"
     // True or False (True by default) - set to False to not use .rsp files
