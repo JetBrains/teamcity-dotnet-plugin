d87bde78faf5fd61a4b806c50a26f916e5132d34
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2020-06-05 14:55:39 +0300

    TW-66365 .NET: add validation for a custom command

--- plugin-dotnet-server/src/main/kotlin/jetbrains/buildServer/dotnet/commands/CustomCommandType.kt
+++ plugin-dotnet-server/src/main/kotlin/jetbrains/buildServer/dotnet/commands/CustomCommandType.kt
@@ -34,4 +34,17 @@ class CustomCommandType : DotnetType() {
     override val editPage: String = "editCustomParameters.jsp"
 
     override val viewPage: String = "viewCustomParameters.jsp"
+
+    override fun validateProperties(properties: Map<String, String>) = sequence {
+        yieldAll(super.validateProperties(properties))
+
+        if (properties[DotnetConstants.PARAM_PATHS].isNullOrBlank() && properties[DotnetConstants.PARAM_ARGUMENTS].isNullOrBlank()) {
+            yield(InvalidProperty(DotnetConstants.PARAM_PATHS, VALIDATION_EMPTY))
+            yield(InvalidProperty(DotnetConstants.PARAM_ARGUMENTS, VALIDATION_EMPTY))
+        }
+    }
+
+    companion object {
+        const val VALIDATION_EMPTY: String = "Either the \"Executables\" field or \"Command line parameters\" should not be empty"
+    }
 }
