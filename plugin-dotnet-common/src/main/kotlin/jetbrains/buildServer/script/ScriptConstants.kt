8e956f7ddee3e9cc2e1be028e1822c459726a367
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2021-07-13 18:50:21 +0300

    TW-72237 Change tool type for the C# runner

--- plugin-dotnet-common/src/main/kotlin/jetbrains/buildServer/script/ScriptConstants.kt
+++ plugin-dotnet-common/src/main/kotlin/jetbrains/buildServer/script/ScriptConstants.kt
@@ -14,7 +14,7 @@ object ScriptConstants {
     const val NUGET_PACKAGE_SOURCES = "nuget.packageSources"
     const val TOOL_PATH = "scriptToolPath"
 
-    const val CLT_TOOL_TYPE_ID = "dotnet-csi"
+    const val CLT_TOOL_TYPE_ID = "TeamCity.csi"
     const val CLT_TOOL_TYPE_NAME = "C# script tool"
 
     const val RUNNER_ENABLED = "teamcity.internal.csharp.script"
