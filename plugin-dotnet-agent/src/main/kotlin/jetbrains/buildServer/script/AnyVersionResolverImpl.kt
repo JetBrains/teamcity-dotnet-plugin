f797a02ed793a431aba01035bdcbe3fe4eb2d59a
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2021-07-14 18:28:42 +0300

    TW-72243 C# script step fails in docker wrapper if agent doesn't have dotnet runtime installed

--- plugin-dotnet-agent/src/main/kotlin/jetbrains/buildServer/script/AnyVersionResolverImpl.kt
+++ plugin-dotnet-agent/src/main/kotlin/jetbrains/buildServer/script/AnyVersionResolverImpl.kt
@@ -1,11 +1,9 @@
 package jetbrains.buildServer.script
 
 import jetbrains.buildServer.RunBuildException
-import jetbrains.buildServer.agent.FileSystemService
-import jetbrains.buildServer.agent.Logger
-import jetbrains.buildServer.agent.ToolProvider
-import jetbrains.buildServer.agent.Version
+import jetbrains.buildServer.agent.*
 import jetbrains.buildServer.dotnet.DotnetConstants
+import jetbrains.buildServer.dotnet.DotnetConstants.RUNNER_DESCRIPTION
 import jetbrains.buildServer.dotnet.DotnetRuntimesProvider
 import jetbrains.buildServer.excluding
 import java.io.File
@@ -14,33 +12,44 @@ import jetbrains.buildServer.to
 
 class AnyVersionResolverImpl(
         private val _fileSystemService: FileSystemService,
-        private val _runtimesProvider: DotnetRuntimesProvider)
+        private val _runtimesProvider: DotnetRuntimesProvider,
+        private val _virtualContext: VirtualContext)
     : AnyVersionResolver {
     override fun resolve(toolsPath: File): File {
-        var runtimes = _runtimesProvider.getRuntimes().map { Version(it.version.major, it.version.minor) }.toList()
-        return _fileSystemService.list(toolsPath)
-                .filter { _fileSystemService.isDirectory(it) }
-                .map {
-                    LOG.debug("Goes through $it")
-                    val version = VersionRegex.matchEntire(it.name)?.let {
-                        Version.parse(it.groupValues[1])
-                    } ?: Version.Empty
+        var supportedRuntimes = getSupportedRuntimes(toolsPath)
 
-                    LOG.debug("Version: $version")
-                    it to version
-                }
-                .filter { it.second != Version.Empty }
-                .filter {
-                    val minVersion = it.second
-                    val runtimeRange = minVersion.including() to Version(minVersion.major + 1).excluding()
-                    val result = runtimes.any {runtimeRange.contains(it)}
-                    LOG.debug("Min version: $minVersion, Runtime range: ${runtimeRange}, Result: $result")
-                    result
-                }
+        if(!_virtualContext.isVirtual) {
+            var runtimes = _runtimesProvider.getRuntimes().map { Version(it.version.major, it.version.minor) }.toList()
+            supportedRuntimes =
+                    supportedRuntimes
+                    .filter {
+                        val minVersion = it.second
+                        val runtimeRange = minVersion.including() to Version(minVersion.major + 1).excluding()
+                        val result = runtimes.any {runtimeRange.contains(it)}
+                        LOG.debug("Min version: $minVersion, Runtime range: ${runtimeRange}, Result: $result")
+                        result
+                    }
+        }
+
+        return supportedRuntimes
                 .maxByOrNull { it.second }
                 ?.first
-                ?: throw RunBuildException("Cannot find a supported versin of C# script runner.")
+                ?: throw RunBuildException("Cannot find a supported version of $RUNNER_DESCRIPTION.")
     }
+
+    private fun getSupportedRuntimes(toolsPath: File) =
+            _fileSystemService.list(toolsPath)
+                    .filter { _fileSystemService.isDirectory(it) }
+                    .map {
+                        LOG.debug("Goes through $it")
+                        val version = VersionRegex.matchEntire(it.name)?.let {
+                            Version.parse(it.groupValues[1])
+                        } ?: Version.Empty
+
+                        LOG.debug("Version: $version")
+                        it to version
+                    }
+                    .filter { it.second != Version.Empty }
 
     companion object {
         private val LOG = Logger.getLogger(ToolResolverImpl::class.java)
