4e06f7cf5ffbe44b46f253def53abd8c4780222b
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2021-08-05 17:06:05 +0300

    TW-72580 System properties serialized incorrectly to the response file

--- plugin-dotnet-agent/src/main/kotlin/jetbrains/buildServer/dotnet/MSBuildArgumentsProvider.kt
+++ plugin-dotnet-agent/src/main/kotlin/jetbrains/buildServer/dotnet/MSBuildArgumentsProvider.kt
@@ -3,14 +3,13 @@ package jetbrains.buildServer.dotnet
 import jetbrains.buildServer.agent.CommandLineArgument
 
 class MSBuildArgumentsProvider(
-        private val _isCommandLineParameters: Boolean,
         private val _msBuildParameterConverter: MSBuildParameterConverter,
         private val _msBuildParametersProviders: List<MSBuildParametersProvider>)
     : ArgumentsProvider {
     override fun getArguments(context: DotnetBuildContext): Sequence<CommandLineArgument> =
             _msBuildParametersProviders.asSequence().flatMap {
                 _msBuildParameterConverter
-                        .convert(it.getParameters(context), _isCommandLineParameters)
+                        .convert(it.getParameters(context))
                         .map { CommandLineArgument(it) }
             }
 
