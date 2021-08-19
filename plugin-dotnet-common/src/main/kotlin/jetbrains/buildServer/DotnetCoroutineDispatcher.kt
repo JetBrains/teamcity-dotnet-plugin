dd8055e4404ba1989662fe32c6099c8dd5809cb2
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2021-08-19 11:35:24 +0300

    Makes Dotnet CoroutineDispatcher closable to shutdown threads from a pool

--- plugin-dotnet-common/src/main/kotlin/jetbrains/buildServer/DotnetCoroutineDispatcher.kt
+++ plugin-dotnet-common/src/main/kotlin/jetbrains/buildServer/DotnetCoroutineDispatcher.kt
@@ -6,19 +6,24 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.ExecutorCoroutineDispatcher
 import kotlinx.coroutines.Runnable
 import kotlinx.coroutines.asCoroutineDispatcher
+import java.io.Closeable
+import java.util.concurrent.ExecutorService
 import java.util.concurrent.ThreadPoolExecutor
 import kotlin.coroutines.CoroutineContext
 
 class DotnetCoroutineDispatcher()
-    : CoroutineDispatcher() {
+    : Closeable, CoroutineDispatcher() {
 
+    private var _executor: ExecutorService
     private var _dispatcher: ExecutorCoroutineDispatcher
 
     init {
-        val executor = ExecutorsFactory.newFixedDaemonExecutor(".NET Runner",  4) as ThreadPoolExecutor
-        _dispatcher = executor.asCoroutineDispatcher()
+        _executor = ExecutorsFactory.newFixedDaemonExecutor(".NET Runner", 4)
+        _dispatcher = _executor.asCoroutineDispatcher()
     }
 
     override fun dispatch(context: CoroutineContext, block: Runnable) =
         _dispatcher.dispatch(context, block)
+
+    override fun close() = _executor.shutdown()
 }
