3a4c7760e8ad0b1e0818c9b581fc1cd3bde1f048
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2021-10-27 19:35:11 +0300

    TW-73773 Linux TeamCity agent running under WSL on windows detects .NET SDKs and runtimes installed on the windows host

--- plugin-dotnet-agent/src/main/kotlin/jetbrains/buildServer/agent/ToolSearchServiceImpl.kt
+++ plugin-dotnet-agent/src/main/kotlin/jetbrains/buildServer/agent/ToolSearchServiceImpl.kt
@@ -16,13 +16,20 @@
 
 package jetbrains.buildServer.agent
 
+import jetbrains.buildServer.util.OSType
 import java.io.File
 
 class ToolSearchServiceImpl(
-        private val _fileSystem: FileSystemService): ToolSearchService {
+        private val _fileSystem: FileSystemService,
+        private val _environment: Environment): ToolSearchService {
 
     override fun find(toolName: String, paths: Sequence<Path>): Sequence<File> {
-        val pattern = Regex("^$toolName(\\.(exe))?$")
+        val executableName = when(_environment.os) {
+            OSType.UNIX, OSType.MAC -> toolName
+            OSType.WINDOWS -> "$toolName.exe"
+        }
+
+        val pattern = Regex("^$executableName$")
         return paths
                 .flatMap { _fileSystem.list(File(it.path)) }
                 .filter { it.name.matches(pattern) }
--- plugin-dotnet-agent/src/test/kotlin/jetbrains/buildServer/dotnet/test/agent/ToolSearchServiceTest.kt
+++ plugin-dotnet-agent/src/test/kotlin/jetbrains/buildServer/dotnet/test/agent/ToolSearchServiceTest.kt
@@ -20,10 +20,8 @@ import io.mockk.MockKAnnotations
 import io.mockk.clearAllMocks
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
-import jetbrains.buildServer.agent.FileSystemService
-import jetbrains.buildServer.agent.Path
-import jetbrains.buildServer.agent.ToolSearchService
-import jetbrains.buildServer.agent.ToolSearchServiceImpl
+import jetbrains.buildServer.agent.*
+import jetbrains.buildServer.util.OSType
 import org.testng.Assert
 import org.testng.annotations.BeforeMethod
 import org.testng.annotations.DataProvider
@@ -32,6 +30,7 @@ import java.io.File
 
 class ToolSearchServiceTest {
     @MockK private lateinit var _fileSystem: FileSystemService
+    @MockK private lateinit var _environment: Environment
 
     @BeforeMethod
     fun setUp() {
@@ -42,26 +41,31 @@ class ToolSearchServiceTest {
     @DataProvider
     fun testData(): Array<Array<Any>> {
         return arrayOf(
-                arrayOf(File("home2", "dotnet"), true),
-                arrayOf(File("home2", "dotnet.exe"), true),
-                arrayOf(File("home1", "dotnet"), false),
-                arrayOf(File("home1", "dotnet.exe"), false),
-                arrayOf(File("home1", "dotnet.a"), false),
-                arrayOf(File("home2", "dotnet.exe"), true),
-                arrayOf(File("home2", "abc.exe"), false),
-                arrayOf(File("home2", "Dotnet.exe"), false),
-                arrayOf(File("home2", "_dotnet.exe"), false),
-                arrayOf(File("home2", "abc_dotnet.exe"), false),
-                arrayOf(File("home2", "dotnet.exea"), false),
-                arrayOf(File("home2", "dotnet.a"), false),
-                arrayOf(File("home2", "dotneta"), false)
+                arrayOf(File("home2", "dotnet"), OSType.UNIX, true),
+                arrayOf(File("home2", "dotnet"), OSType.MAC, true),
+                arrayOf(File("home2", "dotnet"), OSType.WINDOWS, false),
+                arrayOf(File("home2", "dotnet.exe"), OSType.WINDOWS, true),
+                arrayOf(File("home2", "dotnet.exe"), OSType.UNIX, false),
+                arrayOf(File("home2", "dotnet.exe"), OSType.MAC, false),
+                arrayOf(File("home1", "dotnet"), OSType.UNIX, false),
+                arrayOf(File("home1", "dotnet.exe"), OSType.WINDOWS, false),
+                arrayOf(File("home1", "dotnet.a"), OSType.WINDOWS, false),
+                arrayOf(File("home2", "dotnet.exe"), OSType.WINDOWS, true),
+                arrayOf(File("home2", "abc.exe"), OSType.WINDOWS, false),
+                arrayOf(File("home2", "Dotnet.exe"), OSType.WINDOWS, false),
+                arrayOf(File("home2", "_dotnet.exe"), OSType.WINDOWS, false),
+                arrayOf(File("home2", "abc_dotnet.exe"), OSType.WINDOWS, false),
+                arrayOf(File("home2", "dotnet.exea"), OSType.WINDOWS, false),
+                arrayOf(File("home2", "dotnet.a"), OSType.WINDOWS, false),
+                arrayOf(File("home2", "dotneta"), OSType.WINDOWS, false)
         )
     }
 
     @Test(dataProvider = "testData")
-    fun shouldFind(executable: File, success: Boolean) {
+    fun shouldFind(executable: File, os: OSType, success: Boolean) {
         // Given
         val target = "dotnet"
+        every { _environment.os } returns os
         every { _fileSystem.list(File("home1")) } returns emptySequence()
         every { _fileSystem.list(File("home2")) } returns sequenceOf(executable)
 
@@ -74,5 +78,5 @@ class ToolSearchServiceTest {
         Assert.assertEquals(actualTools.contains(executable), success)
     }
 
-    private fun createInstance(): ToolSearchService = ToolSearchServiceImpl(_fileSystem)
+    private fun createInstance(): ToolSearchService = ToolSearchServiceImpl(_fileSystem, _environment)
 }
