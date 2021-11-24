25838394369aa860c8c4f4bd5addce503c0fb917
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2021-11-24 17:14:12 +0300

    TW-74061 C# script runner build is green when C# script tool could not start
    (cherry picked from commit 50676d5876c7921dc988499a5fb011191d228793)

--- plugin-dotnet-agent/src/main/kotlin/jetbrains/buildServer/script/CSharpScriptWorkflowComposer.kt
+++ plugin-dotnet-agent/src/main/kotlin/jetbrains/buildServer/script/CSharpScriptWorkflowComposer.kt
@@ -2,17 +2,38 @@ package jetbrains.buildServer.script
 
 import jetbrains.buildServer.agent.*
 import jetbrains.buildServer.agent.runner.*
+import jetbrains.buildServer.inspect.InspectionArguments
+import jetbrains.buildServer.rx.disposableOf
+import jetbrains.buildServer.rx.filter
+import jetbrains.buildServer.rx.subscribe
+import jetbrains.buildServer.rx.use
+import jetbrains.buildServer.util.OSType
 
 class CSharpScriptWorkflowComposer(
         private val _buildInfo: BuildInfo,
-        private val _commandLineFactory: CommandLineFactory)
+        private val _commandLineFactory: CommandLineFactory,
+        private val _buildOptions: BuildOptions)
     : SimpleWorkflowComposer {
 
     override val target: TargetType = TargetType.Tool
 
     override fun compose(context: WorkflowContext, state: Unit, workflow: Workflow) =
             when (_buildInfo.runType) {
-                ScriptConstants.RUNNER_TYPE -> Workflow(sequenceOf(_commandLineFactory.create()))
+                ScriptConstants.RUNNER_TYPE -> Workflow(createCommandLines(context))
                 else -> Workflow()
             }
+
+    private fun createCommandLines(context: WorkflowContext) = sequence<CommandLine> {
+        var hasErrors = false;
+        val commandLine = _commandLineFactory.create()
+        disposableOf(
+                context.filter { it.SourceId == commandLine.Id }.toExitCodes().subscribe { hasErrors = it != 0; }
+        ).use {
+            yield(commandLine)
+        }
+
+        if (_buildOptions.failBuildOnExitCode && hasErrors) {
+            context.abort(BuildFinishedStatus.FINISHED_FAILED)
+        }
+    }
 }
--- plugin-dotnet-agent/src/test/kotlin/jetbrains/buildServer/dotnet/test/script/CSharpScriptWorkflowComposerTest.kt
+++ plugin-dotnet-agent/src/test/kotlin/jetbrains/buildServer/dotnet/test/script/CSharpScriptWorkflowComposerTest.kt
@@ -1,15 +1,14 @@
 package jetbrains.buildServer.dotnet.test.script
 
-import io.mockk.MockKAnnotations
-import io.mockk.clearAllMocks
-import io.mockk.every
+import io.mockk.*
 import io.mockk.impl.annotations.MockK
-import jetbrains.buildServer.agent.CommandLine
-import jetbrains.buildServer.agent.Path
-import jetbrains.buildServer.agent.TargetType
+import jetbrains.buildServer.agent.*
 import jetbrains.buildServer.agent.runner.BuildInfo
+import jetbrains.buildServer.agent.runner.BuildOptions
 import jetbrains.buildServer.agent.runner.Workflow
 import jetbrains.buildServer.agent.runner.WorkflowContext
+import jetbrains.buildServer.rx.Observer
+import jetbrains.buildServer.rx.emptyDisposable
 import jetbrains.buildServer.script.CSharpScriptWorkflowComposer
 import jetbrains.buildServer.script.CommandLineFactory
 import jetbrains.buildServer.script.ScriptConstants
@@ -22,11 +21,25 @@ class CSharpScriptWorkflowComposerTest {
     @MockK private lateinit var _buildInfo: BuildInfo
     @MockK private lateinit var _commandLineFactory: CommandLineFactory
     @MockK private lateinit var _context: WorkflowContext
+    @MockK private lateinit var _buildOptions: BuildOptions
+    private val _events = mutableListOf<CommandResultEvent>()
 
     @BeforeMethod
     fun setUp() {
         MockKAnnotations.init(this)
         clearAllMocks()
+
+        every { _buildOptions.failBuildOnExitCode } returns true
+        every { _context.abort(BuildFinishedStatus.FINISHED_FAILED) } returns Unit
+
+        every { _context.subscribe(any()) } answers {
+            val observer = arg<Observer<CommandResultEvent>>(0)
+            for (e in _events) {
+                observer.onNext(e)
+            }
+
+            emptyDisposable()
+        }
     }
 
     @Test
@@ -42,6 +55,7 @@ class CSharpScriptWorkflowComposerTest {
 
         // Then
         Assert.assertEquals(actualWorkflow.commandLines.single(), commandLine)
+        verify(exactly = 0) { _context.abort(any()) }
     }
 
     @Test
@@ -57,5 +71,40 @@ class CSharpScriptWorkflowComposerTest {
         Assert.assertEquals(actualWorkflow.commandLines.count(), 0)
     }
 
-    private fun createInstance() = CSharpScriptWorkflowComposer(_buildInfo, _commandLineFactory)
+    @Test
+    fun shouldAbortWhenExitCodeIsNotZero() {
+        // Given
+        val composer = createInstance()
+        every { _buildInfo.runType } returns ScriptConstants.RUNNER_TYPE
+        val commandLine = CommandLine(null, TargetType.Tool, Path("Abc"), Path("Wd"))
+        every { _commandLineFactory.create() } returns commandLine
+
+        // When
+        _events.add(CommandResultExitCode(1, commandLine.Id))
+        val actualWorkflow = composer.compose(_context, Unit, Workflow())
+
+        // Then
+        Assert.assertEquals(actualWorkflow.commandLines.single(), commandLine)
+        verify{ _context.abort(BuildFinishedStatus.FINISHED_FAILED) }
+    }
+
+    @Test
+    fun shouldNotAbortWhenExitCodeIsNotZeroButFailBuildOnExitCodeIsFalse() {
+        // Given
+        val composer = createInstance()
+        every { _buildInfo.runType } returns ScriptConstants.RUNNER_TYPE
+        val commandLine = CommandLine(null, TargetType.Tool, Path("Abc"), Path("Wd"))
+        every { _commandLineFactory.create() } returns commandLine
+
+        // When
+        _events.add(CommandResultExitCode(1, commandLine.Id))
+        every { _buildOptions.failBuildOnExitCode } returns false
+        val actualWorkflow = composer.compose(_context, Unit, Workflow())
+
+        // Then
+        Assert.assertEquals(actualWorkflow.commandLines.single(), commandLine)
+        verify(exactly = 0){ _context.abort(BuildFinishedStatus.FINISHED_FAILED) }
+    }
+
+    private fun createInstance() = CSharpScriptWorkflowComposer(_buildInfo, _commandLineFactory, _buildOptions)
 }
