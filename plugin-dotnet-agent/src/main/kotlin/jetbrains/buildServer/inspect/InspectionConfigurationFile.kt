8fa301b05aa96b31b906a2d1c25d4d4c682b51b9
Vladislav Grinin <grinin.vlad1@gmail.com>
2022-10-24 13:17:38 +0400

    TW-77814 trim solution path [r# inspections]

--- plugin-dotnet-agent/src/main/kotlin/jetbrains/buildServer/inspect/InspectionConfigurationFile.kt
+++ plugin-dotnet-agent/src/main/kotlin/jetbrains/buildServer/inspect/InspectionConfigurationFile.kt
@@ -1,17 +1,14 @@
 package jetbrains.buildServer.inspect
 
 import jetbrains.buildServer.E
-import jetbrains.buildServer.XmlDocumentService
 import jetbrains.buildServer.agent.Path
 import jetbrains.buildServer.agent.runner.ParameterType
 import jetbrains.buildServer.agent.runner.ParametersService
-import jetbrains.buildServer.build
 import jetbrains.buildServer.inspect.InspectCodeConstants.CONFIG_PARAMETER_DISABLE_SOLUTION_WIDE_ANALYSIS
 import jetbrains.buildServer.inspect.InspectCodeConstants.CONFIG_PARAMETER_SUPRESS_BUILD_IN_SETTINGS
 import jetbrains.buildServer.inspect.InspectCodeConstants.RUNNER_SETTING_CUSTOM_SETTINGS_PROFILE_PATH
 import jetbrains.buildServer.inspect.InspectCodeConstants.RUNNER_SETTING_PROJECT_FILTER
 import jetbrains.buildServer.inspect.InspectCodeConstants.RUNNER_SETTING_SOLUTION_PATH
-import org.w3c.dom.Document
 import java.io.OutputStream
 
 class InspectionConfigurationFile(
@@ -31,7 +28,7 @@ class InspectionConfigurationFile(
                         E("Debug", if(debug) debug.toString() else null),
                         E("IncludedProjects", includedProjects.map { E("IncludedProjects", it) } ),
                         E("OutputFile", if(!outputFile.path.isNullOrEmpty()) outputFile.path else null),
-                        E("SolutionFile", _parametersService.tryGetParameter(ParameterType.Runner, RUNNER_SETTING_SOLUTION_PATH)),
+                        E("SolutionFile", _parametersService.tryGetParameter(ParameterType.Runner, RUNNER_SETTING_SOLUTION_PATH)?.trim()),
                         E("CachesHomeDirectory", if(!cachesHomeDirectory?.path.isNullOrEmpty()) cachesHomeDirectory?.path else null),
                         E("CustomSettingsProfile", _parametersService.tryGetParameter(ParameterType.Runner, RUNNER_SETTING_CUSTOM_SETTINGS_PROFILE_PATH)),
                         E("SupressBuildInSettings", _parametersService.tryGetParameter(ParameterType.Runner, CONFIG_PARAMETER_SUPRESS_BUILD_IN_SETTINGS)?.toBoolean()?.toString()),
--- plugin-dotnet-agent/src/test/kotlin/jetbrains/buildServer/dotnet/test/inspect/InspectionConfigurationFileTest.kt
+++ plugin-dotnet-agent/src/test/kotlin/jetbrains/buildServer/dotnet/test/inspect/InspectionConfigurationFileTest.kt
@@ -8,7 +8,6 @@ import jetbrains.buildServer.E
 import jetbrains.buildServer.agent.Path
 import jetbrains.buildServer.agent.runner.ParametersService
 import jetbrains.buildServer.dotnet.test.agent.runner.ParametersServiceStub
-import jetbrains.buildServer.inspect.InspectCodeConstants
 import jetbrains.buildServer.inspect.InspectCodeConstants.CONFIG_PARAMETER_DISABLE_SOLUTION_WIDE_ANALYSIS
 import jetbrains.buildServer.inspect.InspectCodeConstants.CONFIG_PARAMETER_SUPRESS_BUILD_IN_SETTINGS
 import jetbrains.buildServer.inspect.InspectCodeConstants.RUNNER_SETTING_CUSTOM_SETTINGS_PROFILE_PATH
@@ -32,148 +31,187 @@ class InspectionConfigurationFileTest {
     }
 
     @DataProvider(name = "createCases")
-    fun getCreateCases(): Array<Array<out Any?>> {
-        return arrayOf(
-                arrayOf(
-                        Path("output.xml"),
-                        Path("cache"),
-                        true,
-                        ParametersServiceStub(mapOf(
-                                RUNNER_SETTING_PROJECT_FILTER to "Abc\nxyz",
-                                RUNNER_SETTING_SOLUTION_PATH to "My.sln",
-                                CONFIG_PARAMETER_SUPRESS_BUILD_IN_SETTINGS to "true",
-                                CONFIG_PARAMETER_DISABLE_SOLUTION_WIDE_ANALYSIS to "true",
-                                RUNNER_SETTING_CUSTOM_SETTINGS_PROFILE_PATH to "sln.DotSettings"
-                        )),
-                        E("InspectCodeOptions",
-                                E("Debug", "true"),
-                                E("IncludedProjects",
-                                        E("IncludedProjects", "Abc"),
-                                        E("IncludedProjects", "xyz")
-                                ),
-                                E("OutputFile", "output.xml"),
-                                E("SolutionFile", "My.sln"),
-                                E("CachesHomeDirectory", "cache"),
-                                E("CustomSettingsProfile", "sln.DotSettings"),
-                                E("SupressBuildInSettings", "true"),
-                                E("NoSolutionWideAnalysis", "true")
+    fun getCreateCases(): Array<TestCase> {
+        val solutionPathWithSpaces = arrayOf("My.sln ", " My.sln", " My.sln ", "My.sln\n")
+            .map {
+                TestCase(
+                    Path("output.xml"),
+                    null,
+                    false,
+                    ParametersServiceStub(
+                        mapOf(
+                            RUNNER_SETTING_SOLUTION_PATH to it
                         )
-                ),
-                arrayOf(
-                        Path("output.xml"),
-                        null,
-                        true,
-                        ParametersServiceStub(mapOf(
-                                RUNNER_SETTING_PROJECT_FILTER to "Abc\nxyz",
-                                RUNNER_SETTING_SOLUTION_PATH to "My.sln",
-                                CONFIG_PARAMETER_SUPRESS_BUILD_IN_SETTINGS to "true",
-                                CONFIG_PARAMETER_DISABLE_SOLUTION_WIDE_ANALYSIS to "true"
-                        )),
-                        E("InspectCodeOptions",
-                                E("Debug", "true"),
-                                E("IncludedProjects",
-                                        E("IncludedProjects", "Abc"),
-                                        E("IncludedProjects", "xyz")
-                                ),
-                                E("OutputFile", "output.xml"),
-                                E("SolutionFile", "My.sln"),
-                                E("SupressBuildInSettings", "true"),
-                                E("NoSolutionWideAnalysis", "true")
-                        )
-                ),
-                arrayOf(
-                        Path("output.xml"),
-                        Path("cache"),
-                        true,
-                        ParametersServiceStub(mapOf(
-                                RUNNER_SETTING_PROJECT_FILTER to "Abc\nxyz",
-                                RUNNER_SETTING_SOLUTION_PATH to "My.sln",
-                                CONFIG_PARAMETER_SUPRESS_BUILD_IN_SETTINGS to "True",
-                                CONFIG_PARAMETER_DISABLE_SOLUTION_WIDE_ANALYSIS to "True"
-                        )),
-                        E("InspectCodeOptions",
-                                E("Debug", "true"),
-                                E("IncludedProjects",
-                                        E("IncludedProjects", "Abc"),
-                                        E("IncludedProjects", "xyz")
-                                ),
-                                E("OutputFile", "output.xml"),
-                                E("SolutionFile", "My.sln"),
-                                E("CachesHomeDirectory", "cache"),
-                                E("SupressBuildInSettings", "true"),
-                                E("NoSolutionWideAnalysis", "true")
-                        )
-                ),
-                arrayOf(
-                        Path("output.xml"),
-                        Path("cache"),
-                        true,
-                        ParametersServiceStub(mapOf(
-                                RUNNER_SETTING_PROJECT_FILTER to "Abc\nxyz",
-                                RUNNER_SETTING_SOLUTION_PATH to "My.sln"
-                        )),
-                        E("InspectCodeOptions",
-                                E("Debug", true.toString()),
-                                E("IncludedProjects",
-                                        E("IncludedProjects", "Abc"),
-                                        E("IncludedProjects", "xyz")
-                                ),
-                                E("OutputFile", "output.xml"),
-                                E("SolutionFile", "My.sln"),
-                                E("CachesHomeDirectory", "cache")
-                        )
-                ),
-                arrayOf(
-                        Path("output.xml"),
-                        null,
-                        false,
-                        ParametersServiceStub(mapOf(
-                                RUNNER_SETTING_PROJECT_FILTER to "Abc\nxyz",
-                                RUNNER_SETTING_SOLUTION_PATH to "My.sln"
-                        )),
-                        E("InspectCodeOptions",
-                                E("IncludedProjects",
-                                        E("IncludedProjects", "Abc"),
-                                        E("IncludedProjects", "xyz")
-                                ),
-                                E("OutputFile", "output.xml"),
-                                E("SolutionFile", "My.sln")
-                        )
-                ),
-                arrayOf(
-                        Path("Output.xml"),
-                        null,
-                        false,
-                        ParametersServiceStub(emptyMap()),
-                        E("InspectCodeOptions",
-                                E("OutputFile", "Output.xml")
-                        )
+                    ),
+                    E("InspectCodeOptions",
+                        E("OutputFile", "output.xml"),
+                        E("SolutionFile", "My.sln")
+                    )
                 )
+            }
+
+        return arrayOf(
+            TestCase(
+                Path("output.xml"),
+                Path("cache"),
+                true,
+                ParametersServiceStub(
+                    mapOf(
+                        RUNNER_SETTING_PROJECT_FILTER to "Abc\nxyz",
+                        RUNNER_SETTING_SOLUTION_PATH to "My.sln",
+                        CONFIG_PARAMETER_SUPRESS_BUILD_IN_SETTINGS to "true",
+                        CONFIG_PARAMETER_DISABLE_SOLUTION_WIDE_ANALYSIS to "true",
+                        RUNNER_SETTING_CUSTOM_SETTINGS_PROFILE_PATH to "sln.DotSettings"
+                    )
+                ),
+                E("InspectCodeOptions",
+                    E("Debug", "true"),
+                    E("IncludedProjects",
+                        E("IncludedProjects", "Abc"),
+                        E("IncludedProjects", "xyz")
+                    ),
+                    E("OutputFile", "output.xml"),
+                    E("SolutionFile", "My.sln"),
+                    E("CachesHomeDirectory", "cache"),
+                    E("CustomSettingsProfile", "sln.DotSettings"),
+                    E("SupressBuildInSettings", "true"),
+                    E("NoSolutionWideAnalysis", "true")
+                )
+            ),
+
+            TestCase(
+                Path("output.xml"),
+                null,
+                true,
+                ParametersServiceStub(
+                    mapOf(
+                        RUNNER_SETTING_PROJECT_FILTER to "Abc\nxyz",
+                        RUNNER_SETTING_SOLUTION_PATH to "My.sln",
+                        CONFIG_PARAMETER_SUPRESS_BUILD_IN_SETTINGS to "true",
+                        CONFIG_PARAMETER_DISABLE_SOLUTION_WIDE_ANALYSIS to "true"
+                    )
+                ),
+                E("InspectCodeOptions",
+                    E("Debug", "true"),
+                    E("IncludedProjects",
+                        E("IncludedProjects", "Abc"),
+                        E("IncludedProjects", "xyz")
+                    ),
+                    E("OutputFile", "output.xml"),
+                    E("SolutionFile", "My.sln"),
+                    E("SupressBuildInSettings", "true"),
+                    E("NoSolutionWideAnalysis", "true")
+                )
+            ),
+
+            TestCase(
+                Path("output.xml"),
+                Path("cache"),
+                true,
+                ParametersServiceStub(
+                    mapOf(
+                        RUNNER_SETTING_PROJECT_FILTER to "Abc\nxyz",
+                        RUNNER_SETTING_SOLUTION_PATH to "My.sln",
+                        CONFIG_PARAMETER_SUPRESS_BUILD_IN_SETTINGS to "True",
+                        CONFIG_PARAMETER_DISABLE_SOLUTION_WIDE_ANALYSIS to "True"
+                    )
+                ),
+                E("InspectCodeOptions",
+                    E("Debug", "true"),
+                    E("IncludedProjects",
+                        E("IncludedProjects", "Abc"),
+                        E("IncludedProjects", "xyz")
+                    ),
+                    E("OutputFile", "output.xml"),
+                    E("SolutionFile", "My.sln"),
+                    E("CachesHomeDirectory", "cache"),
+                    E("SupressBuildInSettings", "true"),
+                    E("NoSolutionWideAnalysis", "true")
+                )
+            ),
+
+            TestCase(
+                Path("output.xml"),
+                Path("cache"),
+                true,
+                ParametersServiceStub(
+                    mapOf(
+                        RUNNER_SETTING_PROJECT_FILTER to "Abc\nxyz",
+                        RUNNER_SETTING_SOLUTION_PATH to "My.sln"
+                    )
+                ),
+                E("InspectCodeOptions",
+                    E("Debug", true.toString()),
+                    E("IncludedProjects",
+                        E("IncludedProjects", "Abc"),
+                        E("IncludedProjects", "xyz")
+                    ),
+                    E("OutputFile", "output.xml"),
+                    E("SolutionFile", "My.sln"),
+                    E("CachesHomeDirectory", "cache")
+                )
+            ),
+
+            TestCase(
+                Path("output.xml"),
+                null,
+                false,
+                ParametersServiceStub(
+                    mapOf(
+                        RUNNER_SETTING_PROJECT_FILTER to "Abc\nxyz",
+                        RUNNER_SETTING_SOLUTION_PATH to "My.sln"
+                    )
+                ),
+                E("InspectCodeOptions",
+                    E("IncludedProjects",
+                        E("IncludedProjects", "Abc"),
+                        E("IncludedProjects", "xyz")
+                    ),
+                    E("OutputFile", "output.xml"),
+                    E("SolutionFile", "My.sln")
+                )
+            ),
+
+            TestCase(
+                Path("Output.xml"),
+                null,
+                false,
+                ParametersServiceStub(emptyMap()),
+                E("InspectCodeOptions",
+                    E("OutputFile", "Output.xml")
+                )
+            ),
+
+            *solutionPathWithSpaces.toTypedArray()
         )
     }
 
     @Test(dataProvider = "createCases")
-    fun shouldCreate(
-            outputFile: Path,
-            cachesHomeDirectory: Path?,
-            debug: Boolean,
-            parametersService: ParametersService,
-            expctedXml: E?) {
-        // Given
-        val configFile = createInstance(parametersService)
+    fun shouldCreate(case: TestCase) {
+        // arrange
+        val configFile = createInstance(case.parametersService)
         val stream = ByteArrayOutputStream()
-        var actualXml: E? = null;
+        var actualXml: E? = null
         every { _xmlWriter.write(any(), stream) } answers { actualXml = arg<E>(0) }
 
-        // When
-        configFile.create(stream, outputFile, cachesHomeDirectory, debug)
+        // act
+        configFile.create(stream, case.outputFile, case.cachesHomeDirectory, case.debug)
 
-        // Then
-        Assert.assertEquals(actualXml.toString(), expctedXml.toString())
+        // assert
+        Assert.assertEquals(actualXml.toString(), case.expectedXml.toString())
     }
 
     private fun createInstance(parametersService: ParametersService) =
-            InspectionConfigurationFile(
-                    parametersService,
-                    _xmlWriter)
+        InspectionConfigurationFile(
+            parametersService,
+            _xmlWriter
+        )
+
+    data class TestCase(
+        val outputFile: Path,
+        val cachesHomeDirectory: Path?,
+        val debug: Boolean,
+        val parametersService: ParametersService,
+        val expectedXml: E?,
+    )
 }
