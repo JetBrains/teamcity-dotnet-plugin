fabe63ef9f35544ac8cf289ba95f1e3e6b01d3d0
Pavel.Krizskii <pavel.krizskii@jetbrains.com>
2023-06-19 08:34:25 +0000

    TW-80387 Remove test class parameter from test filter expression
    Merge-request: TC-MR-5674
    Merged-by: Pavel Krizskii <Pavel.Krizskii@jetbrains.com>
    (cherry picked from commit 0852b0e89cf930c9c64547c6176d42527a54b9ab)

--- plugin-dotnet-agent/src/test/kotlin/jetbrains/buildServer/dotnet/test/dotnet/commands/test/splitTests/SplitTestsFilterProviderTests.kt
+++ plugin-dotnet-agent/src/test/kotlin/jetbrains/buildServer/dotnet/test/dotnet/commands/test/splitTests/SplitTestsFilterProviderTests.kt
@@ -22,9 +22,12 @@ import jetbrains.buildServer.agent.Logger
 import jetbrains.buildServer.dotnet.commands.test.splitTests.SplitTestsFilterProvider
 import jetbrains.buildServer.dotnet.commands.test.splitTests.SplitTestsFilterSettings
 import jetbrains.buildServer.dotnet.commands.test.splitTests.SplitTestsFilterType
+import jetbrains.buildServer.dotnet.commands.test.splitTests.SplitTestsFilterType.Excludes
+import jetbrains.buildServer.dotnet.commands.test.splitTests.SplitTestsFilterType.Includes
 import jetbrains.buildServer.dotnet.commands.test.splitTests.SplitTestsNamesReader
 import org.testng.Assert
 import org.testng.annotations.BeforeMethod
+import org.testng.annotations.DataProvider
 import org.testng.annotations.Test
 
 class SplitTestsFilterProviderTests {
@@ -43,6 +46,7 @@ class SplitTestsFilterProviderTests {
         MockKAnnotations.init(this)
         mockkObject(Logger)
         every { Logger.getLogger(any()) } returns _loggerMock
+        every { _settingsMock.trimTestClassParameters } returns false
         justRun { _loggerMock.debug(any<String>()) }
         justRun { _loggerMock.warn(any<String>()) }
     }
@@ -58,48 +62,59 @@ class SplitTestsFilterProviderTests {
 
         // assert
         Assert.assertEquals(result, "")
-        verify (exactly = 0) { _loggerMock.debug(any<String>()) }
-        verify (exactly = 0) { _loggerMock.warn(any<String>()) }
+        verify(exactly = 0) { _loggerMock.debug(any<String>()) }
+        verify(exactly = 0) { _loggerMock.warn(any<String>()) }
     }
 
-    @Test
-    fun `should provide default filter expression for includes filter type`() {
+    @DataProvider
+    fun testDataForDefaultFilterExpression() = arrayOf(
+        arrayOf(
+            Includes,
+            sequenceOf("Namespace.TestClass0", "Namespace.TestClass1"),
+            "FullyQualifiedName~Namespace.TestClass0. | FullyQualifiedName~Namespace.TestClass1."
+        ),
+        arrayOf(
+            Excludes,
+            sequenceOf("Namespace.TestClass0", "Namespace.TestClass1"),
+            "FullyQualifiedName!~Namespace.TestClass0. & FullyQualifiedName!~Namespace.TestClass1."
+        ),
+    )
+
+    @Test(dataProvider = "testDataForDefaultFilterExpression")
+    fun `should provide default filter expression`(
+        filterType: SplitTestsFilterType,
+        names: Sequence<String>,
+        expectedFilterExpression: String
+    ) {
         // arrange
         every { _settingsMock.isActive } answers { true }
         every { _settingsMock.useExactMatchFilter } answers { false }
-        every { _settingsMock.filterType } answers { SplitTestsFilterType.Includes }
-        every { _settingsMock.testClasses } answers { generateTestClassesList(2) }
+        every { _settingsMock.filterType } answers { filterType }
+        every { _settingsMock.testClasses } answers { names }
         val provider = create()
 
         // act
         val result = provider.filterExpression
 
         // assert
-        Assert.assertEquals(result, "FullyQualifiedName~Namespace.TestClass0. | FullyQualifiedName~Namespace.TestClass1.")
+        Assert.assertEquals(result, expectedFilterExpression)
     }
 
-    @Test
-    fun `should provide default filter expression for excluded filter type`() {
+    @DataProvider
+    fun testDataForDefaultFilterExpressionWithMoreThan1000TestClasses() = arrayOf(
+        arrayOf(Includes, "^\\(.+\\)\\s{1}\\|\\s{1}\\(.+\\)\\s{1}\\|\\s{1}\\(.+\\)\$"),
+        arrayOf(Excludes, "^\\(.+\\)\\s{1}&\\s{1}\\(.+\\)\\s{1}&\\s{1}\\(.+\\)\$"),
+    )
+
+    @Test(dataProvider = "testDataForDefaultFilterExpressionWithMoreThan1000TestClasses")
+    fun `should provide default filter expression for more than 1000 test classes`(
+        filterType: SplitTestsFilterType,
+        regex: String
+    ) {
         // arrange
         every { _settingsMock.isActive } answers { true }
         every { _settingsMock.useExactMatchFilter } answers { false }
-        every { _settingsMock.filterType } answers { SplitTestsFilterType.Excludes }
-        every { _settingsMock.testClasses } answers { generateTestClassesList(2) }
-        val provider = create()
-
-        // act
-        val result = provider.filterExpression
-
-        // assert
-        Assert.assertEquals(result, "FullyQualifiedName!~Namespace.TestClass0. & FullyQualifiedName!~Namespace.TestClass1.")
-    }
-
-    @Test
-    fun `should provide default filter expression for more than 1000 included test classes`() {
-        // arrange
-        every { _settingsMock.isActive } answers { true }
-        every { _settingsMock.useExactMatchFilter } answers { false }
-        every { _settingsMock.filterType } answers { SplitTestsFilterType.Includes }
+        every { _settingsMock.filterType } answers { filterType }
         every { _settingsMock.testClasses } answers { generateTestClassesList(2500) }
         val provider = create()
 
@@ -107,23 +122,7 @@ class SplitTestsFilterProviderTests {
         val result = provider.filterExpression
 
         // assert
-        Assert.assertTrue(Regex("^\\(.+\\)\\s{1}\\|\\s{1}\\(.+\\)\\s{1}\\|\\s{1}\\(.+\\)\$").matches(result))
-    }
-
-    @Test
-    fun `should provide default filter expression for more than 1000 excluded test classes`() {
-        // arrange
-        every { _settingsMock.isActive } answers { true }
-        every { _settingsMock.useExactMatchFilter } answers { false }
-        every { _settingsMock.filterType } answers { SplitTestsFilterType.Excludes }
-        every { _settingsMock.testClasses } answers { generateTestClassesList(2100) }
-        val provider = create()
-
-        // act
-        val result = provider.filterExpression
-
-        // assert
-        Assert.assertTrue(Regex("^\\(.+\\)\\s{1}&\\s{1}\\(.+\\)\\s{1}&\\s{1}\\(.+\\)\$").matches(result))
+        Assert.assertTrue(Regex(regex).matches(result))
     }
 
     @Test
@@ -160,12 +159,79 @@ class SplitTestsFilterProviderTests {
         Assert.assertTrue(Regex("^\\(.+\\)\\s{1}\\|\\s{1}\\(.+\\)\\s{1}\\|\\s{1}\\(.+\\)\$").matches(result))
     }
 
+    @DataProvider
+    fun testDataForParametrisedTestClasses() = arrayOf(
+        arrayOf(true, Includes, "FullyQualifiedName~Namespace.TestClass0. | FullyQualifiedName~Namespace.TestClass1."),
+        arrayOf(false, Includes, """FullyQualifiedName~Namespace.TestClass0("param"). | FullyQualifiedName~Namespace.TestClass1("param")."""),
+        arrayOf(true, Excludes, "FullyQualifiedName!~Namespace.TestClass0. & FullyQualifiedName!~Namespace.TestClass1."),
+        arrayOf(false, Excludes, """FullyQualifiedName!~Namespace.TestClass0("param"). & FullyQualifiedName!~Namespace.TestClass1("param")."""),
+    )
+
+    @Test(dataProvider = "testDataForParametrisedTestClasses")
+    fun `should provide default filter expression processing test class parameters`(
+        shouldTrimTestClassParameters: Boolean,
+        filterType: SplitTestsFilterType,
+        expectedFilterExpression: String
+    ) {
+        // arrange
+        every { _settingsMock.isActive } answers { true }
+        every { _settingsMock.useExactMatchFilter } answers { false }
+        every { _settingsMock.trimTestClassParameters } answers { shouldTrimTestClassParameters }
+        every { _settingsMock.filterType } answers { filterType }
+        every { _settingsMock.testClasses } answers { generateTestClassesWithParamsList(2) }
+        val provider = create()
+
+        // act
+        val result = provider.filterExpression
+
+        // assert
+        Assert.assertEquals(result, expectedFilterExpression)
+    }
+
+    @DataProvider
+    fun testDataForTrimmingTestClassParameters() = arrayOf(
+        arrayOf("TestClass", "FullyQualifiedName~TestClass."),
+        arrayOf("TestClass(param", "FullyQualifiedName~TestClass(param."),
+        arrayOf("Namespace.TestClass", "FullyQualifiedName~Namespace.TestClass."),
+        arrayOf("Namespace.TestClass(param", "FullyQualifiedName~Namespace.TestClass(param."),
+        arrayOf("Namespace.TestClass(param1,param2)", "FullyQualifiedName~Namespace.TestClass."),
+        arrayOf("Namespace1.Namespace2.TestClass(param1,param2)", "FullyQualifiedName~Namespace1.Namespace2.TestClass."),
+        arrayOf("Namespace.TestClass((param))", "FullyQualifiedName~Namespace.TestClass."),
+        arrayOf("Namespace.TestCla(ss(param)", "FullyQualifiedName~Namespace.TestCla."),
+    )
+
+    @Test(dataProvider = "testDataForTrimmingTestClassParameters")
+    fun `should provide default filter expression processing test class parameters`(
+        sourceName: String,
+        expectedFilterExpression: String
+    ) {
+        // arrange
+        every { _settingsMock.isActive } answers { true }
+        every { _settingsMock.useExactMatchFilter } answers { false }
+        every { _settingsMock.trimTestClassParameters } answers { true }
+        every { _settingsMock.filterType } answers { Includes }
+        every { _settingsMock.testClasses } answers { sequenceOf(sourceName) }
+        val provider = create()
+
+        // act
+        val result = provider.filterExpression
+
+        // assert
+        Assert.assertEquals(result, expectedFilterExpression)
+    }
+
     private fun create() =
-            SplitTestsFilterProvider(_settingsMock, _testsNamesReaderMock)
+        SplitTestsFilterProvider(_settingsMock, _testsNamesReaderMock)
 
     private fun generateTestClassesList(n: Int) = sequence {
         for (index in 0 until n) {
             yield("Namespace.TestClass$index")
+        }
+    }
+
+    private fun generateTestClassesWithParamsList(n: Int) = sequence {
+        for (index in 0 until n) {
+            yield("""Namespace.TestClass$index("param")""")
         }
     }
 
