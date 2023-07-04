b74c49e9bf0c9ba9acd7ea29e62bf3d16f7641d6
Pavel.Krizskii <pavel.krizskii@jetbrains.com>
2023-07-04 07:23:45 +0000

    TW-82299 Set the actual .NET SDK versions in DotNetWorkloads agent parameter and improve logging
     * Previously instead of actual .NET SDK version the "rounded" version was set as the suffix of the parameter name
     * Now the "rounded" version is resolved to actual SDK version
    
    Merge-request: TC-MR-5854
    Merged-by: Pavel Krizskii <Pavel.Krizskii@jetbrains.com>
    (cherry picked from commit 237c568ac9050522f30a9ea5c6761b28dbf86643)

--- plugin-dotnet-agent/src/test/kotlin/jetbrains/buildServer/dotnet/test/dotnet/FileBasedDotnetWorkloadProviderTest.kt
+++ plugin-dotnet-agent/src/test/kotlin/jetbrains/buildServer/dotnet/test/dotnet/FileBasedDotnetWorkloadProviderTest.kt
@@ -2,11 +2,17 @@ package jetbrains.buildServer.dotnet.test.dotnet
 
 import io.mockk.MockKAnnotations
 import io.mockk.clearAllMocks
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
 import jetbrains.buildServer.agent.FileSystemService
 import jetbrains.buildServer.agent.Version
 import jetbrains.buildServer.dotnet.DotnetWorkload
 import jetbrains.buildServer.dotnet.DotnetWorkloadProvider
 import jetbrains.buildServer.dotnet.FileBasedDotnetWorkloadProvider
+import jetbrains.buildServer.dotnet.VersionEnumerator
+import jetbrains.buildServer.dotnet.discovery.dotnetSdk.DotnetSdk
+import jetbrains.buildServer.dotnet.discovery.dotnetSdk.DotnetSdksProvider
 import jetbrains.buildServer.dotnet.test.agent.VirtualFileSystemService
 import org.testng.Assert.assertEquals
 import org.testng.annotations.BeforeMethod
@@ -15,6 +21,13 @@ import org.testng.annotations.Test
 import java.io.File
 
 class FileBasedDotnetWorkloadProviderTest {
+
+    @MockK
+    private lateinit var _sdksProvider: DotnetSdksProvider
+
+    @MockK
+    private lateinit var _versionEnumerator: VersionEnumerator
+
     private val _dotnetExecutable = File("dotnet")
 
     @BeforeMethod
@@ -29,53 +42,75 @@ class FileBasedDotnetWorkloadProviderTest {
             arrayOf(
                 VirtualFileSystemService()
                     .addFile(File("metadata/workloads/6.0.100/InstalledWorkloads/android"))
-                    .addFile(File("metadata/workloads/7.0.100/InstalledWorkloads/android"))
-                    .addFile(File("metadata/workloads/8.0.100/InstalledWorkloads/android"))
+                    .addFile(File("metadata/workloads/7.1.100/InstalledWorkloads/android"))
+                    .addFile(File("metadata/workloads/8.0.100-preview.4/InstalledWorkloads/android"))
                     .addFile(File("metadata/workloads/6.0.100/InstalledWorkloads/ios")),
+                listOf("6.0", "6.0.101", "6.0.102", "7.1", "7.1.100", "8.0.100-preview.4.23260.5", "8.0", "8.0.100"),
+                listOf(
+                    DotnetWorkload("android", Version.parse("6.0.101")),
+                    DotnetWorkload("android", Version.parse("6.0.102")),
+                    DotnetWorkload("android", Version.parse("7.1.100")),
+                    DotnetWorkload("android", Version.parse("8.0.100-preview.4.23260.5")),
+                    DotnetWorkload("ios", Version.parse("6.0.101")),
+                    DotnetWorkload("ios", Version.parse("6.0.102")),
+                )
+            ),
+            arrayOf(
+                VirtualFileSystemService()
+                    .addFile(File("metadata/workloads/6.0.100/InstalledWorkloads/android")),
+                emptyList<String>(),
                 listOf(
                     DotnetWorkload("android", Version.parse("6.0.100")),
-                    DotnetWorkload("android", Version.parse("7.0.100")),
-                    DotnetWorkload("android", Version.parse("8.0.100")),
-                    DotnetWorkload("ios", Version.parse("6.0.100")),
                 )
             ),
             arrayOf(
                 VirtualFileSystemService()
                     .addFile(File("metadata/workloads/6.0.100/InstalledWorkloads/androidDir/file")),
+                emptyList<String>(),
                 emptyList<DotnetWorkload>()
             ),
             arrayOf(
                 VirtualFileSystemService()
                     .addFile(File("metadata/workloads/invalidVersion/InstalledWorkloads/android")),
+                emptyList<String>(),
                 emptyList<DotnetWorkload>()
             ),
             arrayOf(
                 VirtualFileSystemService()
                     .addFile(File("metadata/workloads/InstalledPacks/InstalledWorkloads/android")),
+                emptyList<String>(),
                 emptyList<DotnetWorkload>()
             ),
             arrayOf(
                 VirtualFileSystemService(),
+                emptyList<String>(),
                 emptyList<DotnetWorkload>()
             ),
         )
     }
 
     @Test(dataProvider = "workloadsTestData")
-    fun shouldGetInstalledWorkloads(
+    fun `should get installed workloads`(
         fileSystemService: FileSystemService,
+        sdks: List<String>,
         expectedDotnetWorkloads: Collection<DotnetWorkload>
     ) {
-        // Given
+        // given
         val workloadProvider = createInstance(fileSystemService)
 
-        // When
+        val sdksMock = mockk<Sequence<DotnetSdk>>()
+        every { _sdksProvider.getSdks(_dotnetExecutable) } returns sdksMock
+        every { _versionEnumerator.enumerate(sdksMock) } returns sdks.map {
+            "" to DotnetSdk(_dotnetExecutable, Version.parse(it))
+        }.asSequence()
+
+        // when
         val actualResult = workloadProvider.getInstalledWorkloads(_dotnetExecutable)
 
-        // Then
+        // then
         assertEquals(actualResult, expectedDotnetWorkloads)
     }
 
     private fun createInstance(fileSystemService: FileSystemService): DotnetWorkloadProvider =
-        FileBasedDotnetWorkloadProvider(fileSystemService)
+        FileBasedDotnetWorkloadProvider(fileSystemService, _sdksProvider, _versionEnumerator)
 }
