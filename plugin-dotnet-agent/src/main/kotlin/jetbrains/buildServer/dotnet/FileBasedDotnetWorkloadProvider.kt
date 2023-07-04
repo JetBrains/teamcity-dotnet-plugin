237c568ac9050522f30a9ea5c6761b28dbf86643
Pavel.Krizskii <pavel.krizskii@jetbrains.com>
2023-07-04 07:23:45 +0000

    TW-82299 Set the actual .NET SDK versions in DotNetWorkloads agent parameter and improve logging
     * Previously instead of actual .NET SDK version the "rounded" version was set as the suffix of the parameter name
     * Now the "rounded" version is resolved to actual SDK version
    
    Merge-request: TC-MR-5854
    Merged-by: Pavel Krizskii <Pavel.Krizskii@jetbrains.com>

--- plugin-dotnet-agent/src/main/kotlin/jetbrains/buildServer/dotnet/FileBasedDotnetWorkloadProvider.kt
+++ plugin-dotnet-agent/src/main/kotlin/jetbrains/buildServer/dotnet/FileBasedDotnetWorkloadProvider.kt
@@ -3,37 +3,80 @@ package jetbrains.buildServer.dotnet
 import jetbrains.buildServer.agent.FileSystemService
 import jetbrains.buildServer.agent.Logger
 import jetbrains.buildServer.agent.Version
+import jetbrains.buildServer.dotnet.discovery.dotnetSdk.DotnetSdksProvider
 import java.io.File
 
 class FileBasedDotnetWorkloadProvider(
-    private val _fileSystemService: FileSystemService
+    private val _fileSystemService: FileSystemService,
+    private val _sdksProvider: DotnetSdksProvider,
+    private val _versionEnumerator: VersionEnumerator,
 ) : DotnetWorkloadProvider {
 
     override fun getInstalledWorkloads(dotnetExecutable: File): Collection<DotnetWorkload> {
-        LOG.debug("Trying to get the list of installed workloads for all .NET SDKs from the file system")
+        LOG.debug("Trying to get the list of installed .NET workloads from the file system")
 
         val workloadsPath = File(File(dotnetExecutable.parent, METADATA_DIRECTORY), WORKLOADS_DIRECTORY)
         if (!workloadsPath.isDirectoryExist()) {
-            LOG.debug("The directory <$workloadsPath> does not exist. Seems there are no dotnet workloads installed.")
+            LOG.info(".NET workloads are not found in directory \"$workloadsPath\". The directory does not exist")
             return emptyList()
         }
 
-        return workloadsPath.listChildren()
+        val sdkVersions = _versionEnumerator.enumerate(_sdksProvider.getSdks(dotnetExecutable))
+            .map { it.second.version }
+            .toList()
+
+        val workloads = workloadsPath.listChildren()
             .filter { it.isSdkBandDirectory() }
             .map { it.installedWorkloadsDirectory() }
             .filter { it.isDirectoryExist() }
             .flatMap { it.listChildren() }
             .filter { _fileSystemService.isFile(it) }
-            .map { DotnetWorkload(it.name, Version.parse(sdkBandFromPath(it))) }
+            .flatMap { file ->
+                mapToSdkVersions(Version.parse(workloadSdkFeatureBandFromPath(file)), sdkVersions)
+                    .map { DotnetWorkload(file.name, it) }
+            }
+            .distinct()
             .sortedWith(compareBy(DotnetWorkload::name, DotnetWorkload::sdkVersion))
             .toList()
+
+        logSdksWithNotFoundWorkloads(workloadsPath, sdkVersions, workloads)
+
+        return workloads
     }
 
     private fun File.listChildren() = _fileSystemService.list(this)
     private fun File.isSdkBandDirectory() = isDirectoryExist() && Version.isValid(this.name)
     private fun File.installedWorkloadsDirectory() = File(this, INSTALLED_WORKLOADS_DIRECTORY)
     private fun File.isDirectoryExist() = _fileSystemService.isExists(this) && _fileSystemService.isDirectory(this)
-    private fun sdkBandFromPath(workloadFile: File) = workloadFile.parentFile.parentFile.name
+    private fun workloadSdkFeatureBandFromPath(workloadFile: File) = workloadFile.parentFile.parentFile.name
+
+    private fun mapToSdkVersions(workloadSdkFeatureBand: Version, sdkVersions: List<Version>): List<Version> {
+        return sdkVersions
+            .filter { version -> isTheSameSdkFeatureBand(version, workloadSdkFeatureBand) }
+            .ifEmpty { listOf(workloadSdkFeatureBand) }
+    }
+
+    private fun isTheSameSdkFeatureBand(version: Version, workloadSdkFeatureBand: Version) =
+        version.toString().startsWith(workloadSdkFeatureBand.toString()) || (
+                version.major == workloadSdkFeatureBand.major &&
+                        version.minor == workloadSdkFeatureBand.minor &&
+                        version.release == workloadSdkFeatureBand.release &&
+                        version.patch / 100 == workloadSdkFeatureBand.patch / 100
+                )
+
+    private fun logSdksWithNotFoundWorkloads(
+        workloadsPath: File,
+        sdkVersions: List<Version>,
+        workloads: List<DotnetWorkload>
+    ) {
+        val versionsWithWorkloads = workloads.map { it.sdkVersion }.toSet()
+
+        sdkVersions.forEach { sdkVersion ->
+            if (!versionsWithWorkloads.contains(sdkVersion)) {
+                LOG.info(".NET workloads are not found for .NET SDK $sdkVersion in directory \"$workloadsPath\"")
+            }
+        }
+    }
 
     companion object {
         private val LOG = Logger.getLogger(FileBasedDotnetWorkloadProvider::class.java)
