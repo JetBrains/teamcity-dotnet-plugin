e68014b68f3f169cf40829c440d8186a5d74afe2
Semyon Naumov <semen.naumov@jetbrains.com>
2023-07-10 19:52:31 +0000

    TW-80931: allow selecting credentials provider when runtime roll-forward is possible, make tests clearer
    Merge-request: TC-MR-5909
    Merged-by: Semyon Naumov <Semen.Naumov@jetbrains.com>

--- plugin-dotnet-agent/src/main/kotlin/jetbrains/buildServer/dotnet/commands/nuget/NugetCredentialProviderSelectorImpl.kt
+++ plugin-dotnet-agent/src/main/kotlin/jetbrains/buildServer/dotnet/commands/nuget/NugetCredentialProviderSelectorImpl.kt
@@ -23,55 +23,79 @@ import jetbrains.buildServer.agent.runner.ParametersService
 import jetbrains.buildServer.agent.Logger
 import jetbrains.buildServer.dotnet.DotnetConstants.CONFIG_PREFIX_DOTNET_CREDENTIAL_PROVIDER
 import jetbrains.buildServer.dotnet.DotnetConstants.CONFIG_SUFFIX_PATH
-import jetbrains.buildServer.dotnet.DotnetEnvironmentVariables
 import jetbrains.buildServer.dotnet.discovery.dotnetRuntime.DotnetRuntimesProvider
 
 class NugetCredentialProviderSelectorImpl(
     private val _parametersService: ParametersService,
     private val _runtimesProvider: DotnetRuntimesProvider,
-    private val _virtualContext: VirtualContext)
-    : NugetCredentialProviderSelector {
+    private val _virtualContext: VirtualContext
+) : NugetCredentialProviderSelector {
 
     override fun trySelect(sdkVersion: Version): String? {
-        val credentialproviderDisabled = _parametersService.tryGetParameter(ParameterType.Configuration, "teamcity.nuget.credentialprovider.disabled")?.trim()?.toBoolean() ?: false
-        if (!credentialproviderDisabled) {
-            var majorVersion = sdkVersion.major
-            if (majorVersion == 0) {
-                // for full .NET 4.6 bin\credential-plugin\net46\CredentialProvider.TeamCity.exe
-                majorVersion = 4
-                LOG.debug("Use credentials plugin for full .NET")
-            }
-
-            if (sdkVersion == Version.Empty || sdkVersion >= Version.CredentialProviderVersion) {
-                var credentialproviderPath = _parametersService.tryGetParameter(ParameterType.Configuration, "DotNetCredentialProvider$majorVersion.0.0_Path")
-
-                if (credentialproviderPath == null && sdkVersion != Version.Empty && !_virtualContext.isVirtual) {
-                    LOG.debug("Cannot find .NET SDK version for credentials plugin $majorVersion")
-                    var runtimeVersions = _runtimesProvider.getRuntimes().map { it.version.major }.toSet()
-                    credentialproviderPath =
-                            _parametersService
-                                    .getParameterNames(ParameterType.Configuration)
-                                    .filter { it.startsWith(CONFIG_PREFIX_DOTNET_CREDENTIAL_PROVIDER) }
-                                    .filter { it.endsWith(CONFIG_SUFFIX_PATH) }
-                                    .map { it to Version.parse(it.substring(CONFIG_PREFIX_DOTNET_CREDENTIAL_PROVIDER.length, it.length - CONFIG_SUFFIX_PATH.length)) }
-                                    .filter { it.second != Version.Empty }
-                                    .filter { runtimeVersions.contains(it.second.major) }
-                                    .maxByOrNull { it.second }
-                                    ?.first
-                            ?.let {  _parametersService.tryGetParameter(ParameterType.Configuration, it) }
-                }
-
-                if (credentialproviderPath != null) {
-                    return _virtualContext.resolvePath(credentialproviderPath)
-                }
-            } else {
-                LOG.debug("Credentials plugin is not supported")
-            }
-        } else {
+        val credentialProviderDisabled = _parametersService.tryGetParameter(ParameterType.Configuration, "teamcity.nuget.credentialprovider.disabled")?.trim()?.toBoolean() ?: false
+        if (credentialProviderDisabled) {
             LOG.debug("Credentials plugin is disabled")
+            return null
         }
 
-        return null
+        var sdkMajorVersion = sdkVersion.major
+        if (sdkVersion == Version.Empty) {
+            // for full .NET 4.6 bin\credential-plugin\net46\CredentialProvider.TeamCity.exe
+            sdkMajorVersion = 4
+            LOG.debug("Will use credentials plugin for .NET Framework")
+        } else if (sdkVersion < Version.CredentialProviderVersion) {
+            LOG.debug("Credentials plugin is not supported for SDK version $sdkVersion")
+            return null
+        }
+
+        var credentialProviderPath = trySelectProviderBySdkMajorVersion(sdkMajorVersion)
+        if (credentialProviderPath == null) {
+            LOG.debug("Couldn't find credential plugin for SDK major version $sdkMajorVersion")
+
+            if (sdkVersion != Version.Empty && !_virtualContext.isVirtual) {
+                credentialProviderPath = trySelectProviderByAvailableRuntimes()
+            }
+        }
+
+        if (credentialProviderPath != null) {
+            credentialProviderPath = _virtualContext.resolvePath(credentialProviderPath)
+            LOG.debug("Credentials provider found, using $credentialProviderPath")
+        } else {
+            LOG.debug("Credentials provider not found")
+        }
+
+        return credentialProviderPath
+    }
+
+    private fun trySelectProviderBySdkMajorVersion(sdkMajorVersion: Int): String? {
+        LOG.debug("Selecting credentials plugin by SDK major version $sdkMajorVersion")
+        return _parametersService.tryGetParameter(ParameterType.Configuration, "$CONFIG_PREFIX_DOTNET_CREDENTIAL_PROVIDER$sdkMajorVersion.0.0$CONFIG_SUFFIX_PATH")
+    }
+
+    private fun trySelectProviderByAvailableRuntimes(): String? {
+        val runtimeVersions = _runtimesProvider.getRuntimes().map { it.version.major }.toSet()
+        LOG.debug("Selecting credentials plugin by available runtimes: $runtimeVersions")
+
+        val pluginVersionToNameMap = _parametersService.getParameterNames(ParameterType.Configuration)
+            .filter { it.startsWith(CONFIG_PREFIX_DOTNET_CREDENTIAL_PROVIDER) }
+            .filter { it.endsWith(CONFIG_SUFFIX_PATH) }
+            .filter { _parametersService.tryGetParameter(ParameterType.Configuration, it) != null }
+            .map { Version.parse(it.substring(CONFIG_PREFIX_DOTNET_CREDENTIAL_PROVIDER.length, it.length - CONFIG_SUFFIX_PATH.length)) to it }
+            .filter { it.first != Version.Empty }
+            .associate { it.first to it.second }
+
+        var credentialProviderName = pluginVersionToNameMap
+            .filter { runtimeVersions.contains(it.key.major) }
+            .maxByOrNull { it.key }?.value
+
+        if (credentialProviderName == null) {
+            LOG.debug("Couldn't find pair of matching runtime and credentials plugin, trying to find a roll-forward pair")
+            credentialProviderName = pluginVersionToNameMap
+                .filter { runtimeVersions.any { runtimeVersion -> runtimeVersion > it.key.major } }
+                .maxByOrNull { it.key }?.value
+        }
+
+        return credentialProviderName?.let { _parametersService.tryGetParameter(ParameterType.Configuration, it) }
     }
 
     companion object {
