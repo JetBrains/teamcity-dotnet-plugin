d4ea2840d85cd3f330f570b477602c86a6e1ac33
Boris Yakhno <boris.yakhno@jetbrains.com>
2023-07-17 12:18:16 +0400

    TW-82508 add availability check for DupFinder health report

--- plugin-dotnet-server/src/main/kotlin/jetbrains/buildServer/healthStatus/reports/DupFinderWithIncompatibleCltVersionPageExtension.kt
+++ plugin-dotnet-server/src/main/kotlin/jetbrains/buildServer/healthStatus/reports/DupFinderWithIncompatibleCltVersionPageExtension.kt
@@ -1,8 +1,12 @@
 package jetbrains.buildServer.healthStatus.reports
 
+import jetbrains.buildServer.serverSide.SBuildType
+import jetbrains.buildServer.serverSide.auth.Permission
 import jetbrains.buildServer.web.openapi.PagePlaces
 import jetbrains.buildServer.web.openapi.PluginDescriptor
 import jetbrains.buildServer.web.openapi.healthStatus.HealthStatusItemPageExtension
+import jetbrains.buildServer.web.util.SessionUser
+import javax.servlet.http.HttpServletRequest
 
 class DupFinderWithIncompatibleCltVersionPageExtension(
     pagePlaces: PagePlaces,
@@ -11,5 +15,14 @@ class DupFinderWithIncompatibleCltVersionPageExtension(
     init {
         includeUrl = pluginDescriptor.getPluginResourcesPath("dupFinderWithIncompatibleCltVersionReport.jsp")
         register()
+    }
+
+    override fun isAvailable(request: HttpServletRequest): Boolean {
+        if (!super.isAvailable(request)) return false
+
+        val item = getStatusItem(request)
+        val bt = item.additionalData["buildType"] as SBuildType? ?: return false
+
+        return SessionUser.getUser(request).isPermissionGrantedForProject(bt.projectId, Permission.EDIT_PROJECT)
     }
 }
