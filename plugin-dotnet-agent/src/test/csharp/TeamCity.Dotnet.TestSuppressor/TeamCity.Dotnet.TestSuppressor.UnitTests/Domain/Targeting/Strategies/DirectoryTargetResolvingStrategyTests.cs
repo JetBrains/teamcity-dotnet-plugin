eb9f2c994fbf16ca1714efef87a53e1cbcb5b1c4
Maksim Chechulnikov <maksim.chechulnikov@jetbrains.com>
2023-09-28 19:51:39 +0000

    TW-83955 .NET test suppressor: fix failed unit tests on Windows

--- plugin-dotnet-agent/src/test/csharp/TeamCity.Dotnet.TestSuppressor/TeamCity.Dotnet.TestSuppressor.UnitTests/Domain/Targeting/Strategies/DirectoryTargetResolvingStrategyTests.cs
+++ plugin-dotnet-agent/src/test/csharp/TeamCity.Dotnet.TestSuppressor/TeamCity.Dotnet.TestSuppressor.UnitTests/Domain/Targeting/Strategies/DirectoryTargetResolvingStrategyTests.cs
@@ -8,24 +8,17 @@ namespace TeamCity.Dotnet.TestSuppressor.UnitTests.Domain.Targeting.Strategies;
 
 public class DirectoryTargetResolvingStrategyTests
 {
-    private readonly Mock<ILogger<DirectoryTargetResolvingStrategy>> _loggerMock;
-    private readonly MockFileSystem _fileSystem;
-
-    public DirectoryTargetResolvingStrategyTests()
-    {
-        _loggerMock = new Mock<ILogger<DirectoryTargetResolvingStrategy>>();
-        _fileSystem = new MockFileSystem();
-    }
+    private readonly Mock<ILogger<DirectoryTargetResolvingStrategy>> _loggerMock = new();
+    private readonly MockFileSystem _fileSystem = new();
 
     [Fact]
     public void TestResolve_NoSuchDirectory()
     {
         // arrange
         var strategy = new DirectoryTargetResolvingStrategy(_fileSystem, _loggerMock.Object);
-        const string directoryPath = "/fake/directory";
-            
+
         // act
-        var result = strategy.Resolve(directoryPath).ToList();
+        var result = strategy.Resolve("/fake/directory".ToPlatformPath()).ToList();
 
         // assert
         Assert.Empty(result);
@@ -35,11 +28,11 @@ public class DirectoryTargetResolvingStrategyTests
     public void TestResolve_ValidDirectory_NoProjectsOrSolutions()
     {
         // arrange
-        _fileSystem.AddDirectory("/fake/directory");
+        _fileSystem.AddDirectory("/fake/directory".ToPlatformPath());
         var strategy = new DirectoryTargetResolvingStrategy(_fileSystem, _loggerMock.Object);
             
         // act
-        var result = strategy.Resolve("/fake/directory").ToList();
+        var result = strategy.Resolve("/fake/directory".ToPlatformPath()).ToList();
 
         // assert
         Assert.Empty(result);
@@ -49,15 +42,15 @@ public class DirectoryTargetResolvingStrategyTests
     public void TestResolve_ValidDirectory_WithSolution()
     {
         // arrange
-        _fileSystem.AddFile("/fake/directory/test.sln", new MockFileData(""));
+        _fileSystem.AddFile("/fake/directory/test.sln".ToPlatformPath(), new MockFileData(""));
         var strategy = new DirectoryTargetResolvingStrategy(_fileSystem, _loggerMock.Object);
             
         // act
-        var result = strategy.Resolve("/fake/directory").ToList();
+        var result = strategy.Resolve("/fake/directory".ToPlatformPath()).ToList();
 
         // assert
         Assert.Single(result);
-        Assert.Equal("/fake/directory/test.sln", result.First().Item1.FullName);
+        Assert.Equal("/fake/directory/test.sln".ToPlatformPath(), result.First().Item1.FullName);
         Assert.Equal(TargetType.Solution, result.First().Item2);
     }
 
@@ -65,15 +58,15 @@ public class DirectoryTargetResolvingStrategyTests
     public void TestResolve_ValidDirectory_WithProject()
     {
         // arrange
-        _fileSystem.AddFile("/fake/directory/test.csproj", new MockFileData(""));
+        _fileSystem.AddFile("/fake/directory/test.csproj".ToPlatformPath(), new MockFileData(""));
         var strategy = new DirectoryTargetResolvingStrategy(_fileSystem, _loggerMock.Object);
             
         // act
-        var result = strategy.Resolve("/fake/directory").ToList();
+        var result = strategy.Resolve("/fake/directory".ToPlatformPath()).ToList();
 
         // assert
         Assert.Single(result);
-        Assert.Equal("/fake/directory/test.csproj", result.First().Item1.FullName);
+        Assert.Equal("/fake/directory/test.csproj".ToPlatformPath(), result.First().Item1.FullName);
         Assert.Equal(TargetType.Project, result.First().Item2);
     }
 
@@ -81,15 +74,15 @@ public class DirectoryTargetResolvingStrategyTests
     public void TestResolve_ValidDirectory_WithAssembly()
     {
         // arrange
-        _fileSystem.AddFile("/fake/directory/test.dll", new MockFileData(""));
+        _fileSystem.AddFile("/fake/directory/test.dll".ToPlatformPath(), new MockFileData(""));
         var strategy = new DirectoryTargetResolvingStrategy(_fileSystem, _loggerMock.Object);
             
         // act
-        var result = strategy.Resolve("/fake/directory").ToList();
+        var result = strategy.Resolve("/fake/directory".ToPlatformPath()).ToList();
 
         // assert
         Assert.Single(result);
-        Assert.Equal("/fake/directory/test.dll", result.First().Item1.FullName);
+        Assert.Equal("/fake/directory/test.dll".ToPlatformPath(), result.First().Item1.FullName);
         Assert.Equal(TargetType.Assembly, result.First().Item2);
     }
 }
---
+++ plugin-dotnet-agent/src/test/csharp/TeamCity.Dotnet.TestSuppressor/TeamCity.Dotnet.TestSuppressor.UnitTests/StringExtensions.cs
@@ -0,0 +1,31 @@
+using System.IO.Abstractions;
+
+namespace TeamCity.Dotnet.TestSuppressor.UnitTests;
+
+internal static class StringExtensions
+{
+    private static class PathSeparator
+    {
+        public const char Windows = '\\';
+        public const char Unix = '/';
+    }
+
+    private static class WindowsDrive
+    {
+        public const char C = 'C';
+    }
+
+    /// <summary>
+    /// Handles string as path and normalize it in OS agnostic way
+    /// </summary>
+    public static string ToPlatformPath(this string path, char drive = WindowsDrive.C)
+    {
+        if (Path.DirectorySeparatorChar == PathSeparator.Unix)
+        {
+            return path.Replace(PathSeparator.Windows, PathSeparator.Unix);
+        }
+        
+        var windowsPath = path.Replace(PathSeparator.Unix, PathSeparator.Windows);
+        return path.StartsWith(PathSeparator.Unix.ToString()) ? $"{drive}:{windowsPath}" : windowsPath;
+    }
+}
