09fa7326bea5ab0b22f8fb306ca9e4155b6d117a
Maksim Chechulnikov <maksim.chechulnikov@jetbrains.com>
2023-12-07 12:28:39 +0000

    TW-85181 remove disabling debugger pipes since .NET 8 via stopping passing COMPlus_EnableDiagnostics == 0 env var; slight refactoring `DotnetEnvironmentVariables`
    Merge-request: TC-MR-8198
    Merged-by: Maxim Chechulnikov <Maksim.Chechulnikov@jetbrains.com>

--- plugin-dotnet-agent/src/main/kotlin/jetbrains/buildServer/dotnet/DotnetEnvironmentVariables.kt
+++ plugin-dotnet-agent/src/main/kotlin/jetbrains/buildServer/dotnet/DotnetEnvironmentVariables.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright 2000-2023 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package jetbrains.buildServer.dotnet
 
 import jetbrains.buildServer.agent.CommandLineEnvironmentVariable
@@ -35,38 +19,57 @@ class DotnetEnvironmentVariables(
     private val _loggerResolver: LoggerResolver
 ) : EnvironmentVariables {
     override fun getVariables(sdkVersion: Version): Sequence<CommandLineEnvironmentVariable> = sequence {
-        yieldAll(defaultVariables)
+        yieldAll(sequenceOf(
+            // we don't want to send extra telemetry from an agent to 3rd-party organizations
+            "DOTNET_CLI_TELEMETRY_OPTOUT" to "true",
 
-        yield(CommandLineEnvironmentVariable(MSBUILD_LOGGER_ENV_VAR, _loggerResolver.resolve(ToolType.MSBuild).canonicalPath))
-        yield(CommandLineEnvironmentVariable(VSTEST_LOGGER_ENV_VAR, _loggerResolver.resolve(ToolType.VSTest).canonicalPath))
-        val allowMessagesGuard = _parametersService.tryGetParameter(ParameterType.Configuration, DotnetConstants.PARAM_MESSAGES_GUARD).toBoolean()
-        if (allowMessagesGuard) {
-            yield(CommandLineEnvironmentVariable(SERVICE_MESSAGES_PATH_ENV_VAR, _pathsService.getPath(PathType.AgentTemp).canonicalPath))
+            // disable redundant welcome message of the .NET CLI on an agent
+            "DOTNET_SKIP_FIRST_TIME_EXPERIENCE" to "true",
+
+            // ignore redundant XML files extraction during .nupkg unpacking
+            "NUGET_XMLDOC_MODE" to "skip",
+
+            // pass paths to the TeamCity's MSBuild and VSTest loggers into the build process
+            "TEAMCITY_MSBUILD_LOGGER" to _loggerResolver.resolve(ToolType.MSBuild).canonicalPath,
+            "TEAMCITY_VSTEST_LOGGER" to _loggerResolver.resolve(ToolType.VSTest).canonicalPath,
+
+            // disable shared compilation by default; enable if the user-defined variable is present
+            USE_SHARED_COMPILATION_ENV_VAR to useSharedCompilation.toString(),
+        ))
+
+        // set user home directory from Java system properties if it's not defined by user
+        if (_environment.tryGetVariable(userHomeEnvVar).isNullOrBlank()) {
+            yield(userHomeEnvVar to System.getProperty("user.home"))
         }
 
-        val useSharedCompilationParameter = _parametersService.tryGetParameter(ParameterType.Environment, USE_SHARED_COMPILATION_ENV_VAR)
-        val useSharedCompilation = if (useSharedCompilationParameter?.equals("true", true) == true) "true" else "false"
-        yield(CommandLineEnvironmentVariable(USE_SHARED_COMPILATION_ENV_VAR, useSharedCompilation))
-        yieldAll(_additionalEnvironmentVariables.flatMap { it.getVariables(sdkVersion) })
-
-        val home = if (_environment.os == OSType.WINDOWS) USER_PROFILE_ENV_VAR else HOME_ENV_VAR
-        if (_environment.tryGetVariable(home).isNullOrEmpty()) {
-            yield(CommandLineEnvironmentVariable(home, System.getProperty("user.home")))
+        // disable debugger pipes creation in temp directory: https://youtrack.jetbrains.com/issue/TW-60571
+        // applies till .NET 8 version only since it raises an issue with dotCover: https://youtrack.jetbrains.com/issue/TW-85181
+        if (sdkVersion < Dotnet8Version) {
+            yield("COMPlus_EnableDiagnostics" to "0")
         }
+
+        // set service messages backup file path if allowed
+        if (allowMessageGuard) {
+            yield("TEAMCITY_SERVICE_MESSAGES_PATH" to _pathsService.getPath(PathType.AgentTemp).canonicalPath)
+        }
+    }
+        .map { CommandLineEnvironmentVariable(it.first, it.second) }
+        .let { it + _additionalEnvironmentVariables.flatMap { it.getVariables(sdkVersion) }}
+
+    private val useSharedCompilation get() =
+        _parametersService.tryGetParameter(ParameterType.Environment, USE_SHARED_COMPILATION_ENV_VAR)
+            ?.trim().toBoolean()
+
+    private val allowMessageGuard get() =
+        _parametersService.tryGetParameter(ParameterType.Configuration, DotnetConstants.PARAM_MESSAGES_GUARD)
+            ?.trim().toBoolean()
+
+    private val userHomeEnvVar get() = when {
+        _environment.os == OSType.WINDOWS -> "USERPROFILE"
+        else -> "HOME"
     }
 
     companion object {
-        private const val USER_PROFILE_ENV_VAR = "USERPROFILE"
-        private const val HOME_ENV_VAR = "HOME"
-        internal const val MSBUILD_LOGGER_ENV_VAR = "TEAMCITY_MSBUILD_LOGGER"
-        internal const val VSTEST_LOGGER_ENV_VAR = "TEAMCITY_VSTEST_LOGGER"
-        internal const val SERVICE_MESSAGES_PATH_ENV_VAR = "TEAMCITY_SERVICE_MESSAGES_PATH"
-
-        internal val defaultVariables = sequenceOf(
-            CommandLineEnvironmentVariable("COMPlus_EnableDiagnostics", "0"),
-            CommandLineEnvironmentVariable("DOTNET_CLI_TELEMETRY_OPTOUT", "true"),
-            CommandLineEnvironmentVariable("DOTNET_SKIP_FIRST_TIME_EXPERIENCE", "true"),
-            CommandLineEnvironmentVariable("NUGET_XMLDOC_MODE", "skip")
-        )
+        private val Dotnet8Version = Version(8,0,0)
     }
 }
