5eff2045553a95965e9d1abf40aae2eff4e9620f
Maksim Chechulnikov <maksim.chechulnikov@jetbrains.com>
2024-03-12 00:14:08 +0100

    TW-86602 fix comments

--- plugin-dotnet-common/src/main/kotlin/jetbrains/buildServer/dotnet/DotnetConstants.kt
+++ plugin-dotnet-common/src/main/kotlin/jetbrains/buildServer/dotnet/DotnetConstants.kt
@@ -64,9 +64,9 @@ object DotnetConstants {
     const val PARAM_DOTCOVER_WRAPPER_MERGE_ENABLED = "teamcity.internal.dotnet.dotCover.wrapper.mergeEnabled"
     // true or false (true by default) - automatically generate report in the dotCover wrapper option is enabled
     const val PARAM_DOTCOVER_WRAPPER_REPORT_ENABLED = "teamcity.internal.dotnet.dotCover.wrapper.reportEnabled"
-    // true or false (true by default) - automatically generate report in the dotCover wrapper option is enabled
+    // true or false (true by default) - add default exclude assembly filters for dotCover
     const val PARAM_DOTCOVER_WRAPPER_COVER_DEFAULT_ASSEMBLY_FILTERS_ENABLED = "teamcity.internal.dotnet.dotCover.wrapper.defaultExcludeAssemblyFiltersEnabled"
-    // true or false (true by default) - automatically generate report in the dotCover wrapper option is enabled
+    // true or false (true by default) - add default exclude attribute filters for dotCover
     const val PARAM_DOTCOVER_WRAPPER_COVER_DEFAULT_ATTRIBUTE_FILTERS_ENABLED = "teamcity.internal.dotnet.dotCover.wrapper.defaultExcludeAttributeFiltersEnabled"
     // true or false (true by default)
     const val PARAM_DOTCOVER_TEMP_DIR_OVERRIDE = "teamcity.internal.dotcover.temp.directory.override"
