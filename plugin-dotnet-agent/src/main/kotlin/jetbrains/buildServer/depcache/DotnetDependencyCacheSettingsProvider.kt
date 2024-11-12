17c53bb6dbfbc8d9d23511397cf829f70e411a31
Semyon Naumov <semen.naumov@jetbrains.com>
2024-11-12 18:40:03 +0000

    TW-90316: Adapt to changes in RunnerDependencyCacheSettingsProvider
    Merge-request: TC-MR-12696
    Merged-by: Semyon Naumov <Semen.Naumov@jetbrains.com>

--- plugin-dotnet-agent/src/main/kotlin/jetbrains/buildServer/depcache/DotnetDependencyCacheSettingsProvider.kt
+++ plugin-dotnet-agent/src/main/kotlin/jetbrains/buildServer/depcache/DotnetDependencyCacheSettingsProvider.kt
@@ -1,11 +1,11 @@
 package jetbrains.buildServer.depcache
 
 import jetbrains.buildServer.agent.AgentLifeCycleListener
+import jetbrains.buildServer.agent.AgentRunningBuild
+import jetbrains.buildServer.agent.BuildFinishedStatus
 import jetbrains.buildServer.agent.cache.depcache.DependencyCacheProvider
 import jetbrains.buildServer.agent.cache.depcache.DependencyCacheSettingsProviderRegistry
-import jetbrains.buildServer.agent.cache.depcache.buildFeature.RunnerDependencyCacheSettingsProvider
-import jetbrains.buildServer.agent.cache.depcache.cacheroot.CacheRootPublishPaths
-import jetbrains.buildServer.agent.cache.depcache.cacheroot.CacheRootPublisher
+import jetbrains.buildServer.agent.cache.depcache.buildFeature.BuildRunnerDependencyCacheSettingsProvider
 import jetbrains.buildServer.dotnet.DotnetConstants
 import jetbrains.buildServer.util.EventDispatcher
 
@@ -13,7 +13,7 @@ class DotnetDependencyCacheSettingsProvider(
     private val eventDispatcher: EventDispatcher<AgentLifeCycleListener>,
     private val cacheSettingsProviderRegistry: DependencyCacheSettingsProviderRegistry,
     private val cacheProvider: DependencyCacheProvider
-) : RunnerDependencyCacheSettingsProvider<DotnetPackagesChangedInvalidator>(
+) : BuildRunnerDependencyCacheSettingsProvider(
     eventDispatcher, cacheSettingsProviderRegistry, cacheProvider,
     DotnetConstants.RUNNER_TYPE,
     DotnetConstants.RUNNER_DISPLAY_NAME,
@@ -21,14 +21,15 @@ class DotnetDependencyCacheSettingsProvider(
     DotnetDependencyCacheConstants.FEATURE_TOGGLE_DOTNET_DEPENDENCY_CACHE,
     DotnetDependencyCacheConstants.FEATURE_TOGGLE_DOTNET_DEPENDENCY_CACHE_DEFAULT
 ) {
+    var postBuildInvalidator: DotnetPackagesChangedInvalidator? = null
+        private set
 
-    protected override fun createPostBuildInvalidator(): DotnetPackagesChangedInvalidator {
-        return DotnetPackagesChangedInvalidator()
+    protected override fun createPostBuildInvalidators(): List<DotnetPackagesChangedInvalidator> {
+        postBuildInvalidator = DotnetPackagesChangedInvalidator()
+        return listOf(postBuildInvalidator!!)
     }
 
-    protected override fun createCacheRootPublisher(): CacheRootPublisher {
-        return CacheRootPublisher({
-            CacheRootPublishPaths.includeAll()
-        })
+    public override fun buildFinished(build: AgentRunningBuild, buildStatus: BuildFinishedStatus) {
+        postBuildInvalidator = null
     }
 }
