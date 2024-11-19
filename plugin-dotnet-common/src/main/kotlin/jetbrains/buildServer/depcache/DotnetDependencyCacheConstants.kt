e581c1e88c71a74253548374a6c9c6b5ec577b88
Vitaliy Kim <vitaliy.kim@jetbrains.com>
2024-11-19 13:54:24 +0100

    TW-90901 [Nuget Dependency cache] Enable build feature by default

--- plugin-dotnet-common/src/main/kotlin/jetbrains/buildServer/depcache/DotnetDependencyCacheConstants.kt
+++ plugin-dotnet-common/src/main/kotlin/jetbrains/buildServer/depcache/DotnetDependencyCacheConstants.kt
@@ -4,5 +4,5 @@ object DotnetDependencyCacheConstants {
     const val CACHE_ROOT_TYPE: String = "nuget-packages"
     const val CACHE_DISPLAY_NAME: String = "NuGet"
     const val FEATURE_TOGGLE_DOTNET_DEPENDENCY_CACHE: String = "teamcity.internal.depcache.buildFeature.dotnet.enabled"
-    const val FEATURE_TOGGLE_DOTNET_DEPENDENCY_CACHE_DEFAULT: Boolean = false
+    const val FEATURE_TOGGLE_DOTNET_DEPENDENCY_CACHE_DEFAULT: Boolean = true
 }
