5da382959aa7622a72ea3eb6dd6adffb5db43d3e
Semyon Naumov <semen.naumov@jetbrains.com>
2025-03-18 15:29:57 +0000

    TW-92608: Support .NET 8+ in C# script tool: update agent requirements, allow runtime roll-forward during tool assembly selection to support major roll-forward in tool version 1.0.7 and above.
    Merge-request: TC-MR-14758
    Merged-by: Semyon Naumov <Semen.Naumov@jetbrains.com>

--- plugin-dotnet-agent/src/main/kotlin/jetbrains/buildServer/script/ToolVersionResolverImpl.kt
+++ plugin-dotnet-agent/src/main/kotlin/jetbrains/buildServer/script/ToolVersionResolverImpl.kt
@@ -1,5 +1,3 @@
-
-
 package jetbrains.buildServer.script
 
 import jetbrains.buildServer.RunBuildException
@@ -14,27 +12,35 @@ import jetbrains.buildServer.to
 class ToolVersionResolverImpl(
     private val _fileSystemService: FileSystemService,
     private val _runtimesProvider: DotnetRuntimesProvider,
-    private val _virtualContext: VirtualContext)
-    : ToolVersionResolver {
+    private val _virtualContext: VirtualContext
+) : ToolVersionResolver {
     override fun resolve(toolPath: File): CsiTool {
         var supportedRuntimes = getSupportedRuntimes(toolPath).toList()
 
         var tool: CsiTool?
-        if(!_virtualContext.isVirtual) {
-            var runtimes = _runtimesProvider.getRuntimes().map { Version(it.version.major, it.version.minor) }.distinct().toList()
-            supportedRuntimes =
-                    supportedRuntimes
-                    .filter {
-                        val minVersion = it.runtimeVersion
-                        val runtimeRange = minVersion.including() to Version(minVersion.major + 1).excluding()
-                        val result = runtimes.any {runtimeRange.contains(it)}
-                        LOG.debug("Min version: $minVersion, Runtime range: ${runtimeRange}, Result: $result")
-                        result
-                    }
+        if (!_virtualContext.isVirtual) {
+            var agentRuntimes = _runtimesProvider.getRuntimes().map { Version(it.version.major, it.version.minor) }.distinct().toList()
 
-            tool  = supportedRuntimes.maxByOrNull { it.runtimeVersion }
-        }
-        else {
+            // try to find major match first
+            tool = supportedRuntimes.filter {
+                val runtimeVersion = it.runtimeVersion
+                val runtimeRange = runtimeVersion.including() to Version(runtimeVersion.major + 1).excluding()
+                val anyAgentRuntimeMatches = agentRuntimes.any { runtimeRange.contains(it) }
+                LOG.debug("[Major matching] version: $runtimeVersion, range: ${runtimeRange}, any agent runtime matches: $anyAgentRuntimeMatches")
+                anyAgentRuntimeMatches
+            }.maxByOrNull { it.runtimeVersion }
+
+            // fallback to major roll-forward if no matches found
+            if (tool == null) {
+                tool = supportedRuntimes.filter {
+                    val runtimeVersion = it.runtimeVersion
+                    val runtimeRange = runtimeVersion.including() to Version(Int.MAX_VALUE).including()
+                    val anyAgentRuntimeMatches = agentRuntimes.any { runtimeRange.contains(it) }
+                    LOG.debug("[Major roll-forward matching] version: $runtimeVersion, range: ${runtimeRange}, any agent runtime matches: $anyAgentRuntimeMatches")
+                    anyAgentRuntimeMatches
+                }.maxByOrNull { it.runtimeVersion }
+            }
+        } else {
             // Should return .NET Runtime LTS version for docker 6.0.0, 8.0.0 etc. according to https://dotnet.microsoft.com/en-us/platform/support/policy
             // or any newest available version
             tool = supportedRuntimes.filter { it.runtimeVersion.major % 2 == 0 }.maxByOrNull { it.runtimeVersion } ?: supportedRuntimes.maxByOrNull { it.runtimeVersion }
@@ -44,18 +50,18 @@ class ToolVersionResolverImpl(
     }
 
     private fun getSupportedRuntimes(toolsPath: File) =
-            _fileSystemService.list(toolsPath)
-                    .filter { _fileSystemService.isDirectory(it) }
-                    .map {
-                        LOG.debug("Goes through $it")
-                        val version = VersionRegex.matchEntire(it.name)?.let {
-                            Version.parse(it.groupValues[1])
-                        } ?: Version.Empty
+        _fileSystemService.list(toolsPath)
+            .filter { _fileSystemService.isDirectory(it) }
+            .map {
+                LOG.debug("Goes through $it")
+                val version = VersionRegex.matchEntire(it.name)?.let {
+                    Version.parse(it.groupValues[1])
+                } ?: Version.Empty
 
-                        LOG.debug("Version: $version")
-                        CsiTool(it, version)
-                    }
-                    .filter { it.runtimeVersion != Version.Empty }
+                LOG.debug("Version: $version")
+                CsiTool(it, version)
+            }
+            .filter { it.runtimeVersion != Version.Empty }
 
     companion object {
         private val LOG = Logger.getLogger(ToolResolverImpl::class.java)
