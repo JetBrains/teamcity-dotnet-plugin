9dca50d0645e322c76fad97171d9a3c2f311545b
Semyon Naumov <semen.naumov@jetbrains.com>
2026-03-18 19:24:50 +0000

    TW-87329: Add more descriptive error message when processing ReSharper Inspectcode report failed
    Merge-request: TC-MR-21403
    Merged-by: Semyon Naumov <Semen.Naumov@jetbrains.com>

--- plugin-dotnet-agent/src/main/kotlin/jetbrains/buildServer/inspect/InspectCodeDataProcessor.kt
+++ plugin-dotnet-agent/src/main/kotlin/jetbrains/buildServer/inspect/InspectCodeDataProcessor.kt
@@ -1,57 +1,61 @@
-
-
 package jetbrains.buildServer.inspect
 
+import jetbrains.buildServer.RunBuildException
 import jetbrains.buildServer.agent.DataProcessor
 import jetbrains.buildServer.agent.DataProcessorContext
 import jetbrains.buildServer.agent.FileSystemService
 import jetbrains.buildServer.agent.inspections.*
 
 class InspectCodeDataProcessor(
-        private val _fileSystem: FileSystemService,
-        private val _xmlReader: XmlReader,
-        private val _reporter: InspectionReporter)
-    : DataProcessor {
+    private val _fileSystem: FileSystemService,
+    private val _xmlReader: XmlReader,
+    private val _reporter: InspectionReporter
+) : DataProcessor {
     override fun getType() = InspectCodeConstants.DATA_PROCESSOR_TYPE
 
     override fun processData(context: DataProcessorContext) {
         _reporter.markBuildAsInspectionsBuild()
         _fileSystem.read(context.file) {
             val inspectionTypeSeverities = mutableMapOf<String, InspectionSeverityValues>()
-            for (e in _xmlReader.read(it)) {
-                when (e.name.lowercase()) {
-                    "issuetype" -> {
-                        val severity = when (e["Severity"]?.lowercase()) {
-                            "error" -> InspectionSeverityValues.ERROR
-                            "warning" -> InspectionSeverityValues.WARNING
-                            else -> InspectionSeverityValues.INFO
+
+            try {
+                for (e in _xmlReader.read(it)) {
+                    when (e.name.lowercase()) {
+                        "issuetype" -> {
+                            val severity = when (e["Severity"]?.lowercase()) {
+                                "error" -> InspectionSeverityValues.ERROR
+                                "warning" -> InspectionSeverityValues.WARNING
+                                else -> InspectionSeverityValues.INFO
+                            }
+
+                            val type = InspectionTypeInfo()
+                            type.id = e["Id"]
+                            type.name = e["Description"]
+                            type.category = e["Category"]
+                            type.description = e["WikiUrl"] ?: "" //optional
+                            if (type.id.isNotBlank()) {
+                                inspectionTypeSeverities[type.id] = severity
+                                _reporter.reportInspectionType(type)
+                            }
                         }
 
-                        val type = InspectionTypeInfo()
-                        type.id = e["Id"]
-                        type.name = e["Description"]
-                        type.category = e["Category"]
-                        type.description = e["WikiUrl"] ?: "" //optional
-                        if (type.id.isNotBlank()) {
-                            inspectionTypeSeverities[type.id] = severity
-                            _reporter.reportInspectionType(type)
-                        }
-                    }
-
-                    "issue" -> {
-                        val info = InspectionInstance()
-                        info.inspectionId = e["TypeId"]
-                        info.filePath = e["File"]?.replace("\\", "/")
-                        info.line = e["Line"]?.toIntOrNull() ?: 0
-                        info.message = e["Message"]
-                        if (info.inspectionId.isNotBlank()) {
-                            inspectionTypeSeverities[info.inspectionId]?.let { severity ->
-                                info.addAttribute(InspectionAttributesId.SEVERITY.toString(), listOf(severity.toString()))
-                                _reporter.reportInspection(info)
+                        "issue" -> {
+                            val info = InspectionInstance()
+                            info.inspectionId = e["TypeId"]
+                            info.filePath = e["File"]?.replace("\\", "/")
+                            info.line = e["Line"]?.toIntOrNull() ?: 0
+                            info.message = e["Message"]
+                            if (info.inspectionId.isNotBlank()) {
+                                inspectionTypeSeverities[info.inspectionId]?.let { severity ->
+                                    info.addAttribute(InspectionAttributesId.SEVERITY.toString(), listOf(severity.toString()))
+                                    _reporter.reportInspection(info)
+                                }
                             }
                         }
                     }
                 }
+            } catch (e: Exception) {
+                throw RunBuildException("Failed to read ReSharper Inspectcode report. Please make sure the report format is set to XML (SARIF is not supported yet).", e)
             }
         }
     }
